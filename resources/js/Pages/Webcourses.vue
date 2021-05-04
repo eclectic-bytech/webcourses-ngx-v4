3f9af7d118fc91bf2b5a55933fd19c01b5b53038
Sławomir Borkowski <slawomir@bytech.ca>
2021-05-04 20:30:08 +0200

    added webcourses page in user / vue

---
+++ resources/js/Pages/Webcourses.vue
@@ -0,0 +1,35 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                My Web Courses
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="text-2xl capitalize p-6">
+                        Hello, {{ $page.props.user.name }}.
+                    </div>
+                    <div class="text-gray-500 p-6">
+                        Here you'll find all courses you've access to.
+                    </div>
+                    <!-- <welcome /> -->
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    import Welcome from '@/Jetstream/Welcome'
+
+    export default {
+        components: {
+            AppLayout,
+            Welcome,
+        },
+    }
+</script>
