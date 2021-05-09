4be74454a5661d78b5828edbd4285e39b4453435
Sławomir Borkowski <slawomir@bytech.ca>
2021-05-09 10:55:18 +0200

    reverted changes in api.php. it now should be what it was after jetstream was installed.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
