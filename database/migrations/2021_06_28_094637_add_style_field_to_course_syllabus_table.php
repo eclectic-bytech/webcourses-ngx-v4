28747df2743d97e10f8d4829ca582542545e40f1
vagrant <vagrant@homestead>
2021-06-28 10:38:54 +0000

    migration with style field add to couse_syllabus table

---
+++ database/migrations/2021_06_28_094637_add_style_field_to_course_syllabus_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStyleFieldToCourseSyllabusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('course_syllabus', function (Blueprint $table) {
+            $table->string('style', 32)->after('default_answer');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('course_syllabus', function (Blueprint $table) {
+            //
+        });
+    }
+}
