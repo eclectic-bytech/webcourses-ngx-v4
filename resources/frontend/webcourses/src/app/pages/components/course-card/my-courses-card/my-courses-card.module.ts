e1363d494d534d63c8a027015ddc4bc21e2a3973
Sławomir Borkowski <slawomir@bytech.ca>
2021-07-23 19:27:16 +0200

    mycourses module and safehtml pipe

---
+++ resources/frontend/webcourses/src/app/pages/components/course-card/my-courses-card/my-courses-card.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { MyCoursesCardComponent } from './my-courses-card.component'
+import { PipesModule } from './../../../../shared/pipes/pipes.module'
+
+@NgModule({
+  declarations: [
+    MyCoursesCardComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    PipesModule
+  ]
+})
+
+export class MyCoursesCardModule { }
