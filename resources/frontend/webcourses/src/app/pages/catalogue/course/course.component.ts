3ad35cc6817d15864f17b40a790859e8e5dcc2d4
Sławomir Borkowski <slawomir@bytech.ca>
2021-09-16 17:37:37 +0200

    replaced publisher service in course component and template with course.publisher

--- resources/frontend/webcourses/src/app/pages/catalogue/course/course.component.ts
+++ resources/frontend/webcourses/src/app/pages/catalogue/course/course.component.ts
@@ -3,8 +3,6 @@ import { Observable } from 'rxjs'
 import { ActivatedRoute } from '@angular/router'
 
 import { CourseService } from './course.service'
-import { PublisherService } from '../publisher/publisher.service'
-import { PublisherModel } from '../../catalogue/models/publisher.model'
 import { FadeInOut } from '../../../core/animations/fade-in-out.animation'
 import { Course } from 'src/app/models/course.model'
 
@@ -17,21 +15,16 @@ import { Course } from 'src/app/models/course.model'
 })
 export class CourseComponent implements OnInit {
 
-  // public publisherInfo$: Observable<PublisherModel>
   public courseInfo$: Observable<Course>
 
   constructor(
     private courseService: CourseService,
-    private publisherService: PublisherService,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(
       params => {
-        // this.publisherInfo$ = this.publisherService.getPublisher(params.cid, 'cid').pipe(
-        //   ( publisherInfo => publisherInfo)
-        // )
         this.courseInfo$ = this.courseService.getCourse(params.cid).pipe(
           ( course => course )
         )
