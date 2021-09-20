19bdb04a0c3d14f46fd2856e3fe3940d76dc0849
Sławomir Borkowski <support@webcourses.io>
2021-09-20 18:27:37 -0500

    squash merged dev

---
+++ app/Http/Controllers/ActivityController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Activity;
+
+class ActivityController extends Controller
+{
+    public function activity($aid = false) {
+        return Activity::where('id', $aid)->get();
+    }
+}
