4e8ee815a8a7c4d78888a042cf71cb5ac604f07c
Sławomir Borkowski <support@webcourses.io>
2021-09-21 12:41:15 -0500

    updated activityController's activity method to include meta and answers. adde activity and answer models.

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -8,6 +8,9 @@ use App\Models\Activity;
 class ActivityController extends Controller
 {
     public function activity($aid = false) {
-        return Activity::where('id', $aid)->first();
+        return Activity
+            ::where('id', $aid)
+            ->with('meta', 'answers')
+            ->first();
     }
 }
