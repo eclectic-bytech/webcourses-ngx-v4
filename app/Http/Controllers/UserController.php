e3ccf9dcf9e263a3dcebde0b6a1dc9aae0abea58
Sławomir Borkowski <support@webcourses.io>
2021-09-22 00:13:19 -0500

    added save_name method to userController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+
 
 class UserController extends Controller
 {
@@ -11,7 +13,13 @@ class UserController extends Controller
         return auth()->user();
     }
 
-    public function save_name() {
-        return "Lenoczka Novikova";
+    public function save_name(Request $request) {
+        $input = $request->input();
+        $user = auth()->user();
+        $name = $input['firstName'] . ' ' . $input['lastName'];
+
+        return User
+            ::where('id', $user['id'])
+            ->update(['name' => $name]);
     }
 }
