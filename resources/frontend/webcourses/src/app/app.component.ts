b493eb36a2907a14224c503a5836f2b156a06b32
Sławomir Borkowski <slawomir@bytech.ca>
2021-09-23 19:19:29 -0500

    implemented servicePrimer in app ngInit. updated selected component/template to make use of the selectedCourses service

--- resources/frontend/webcourses/src/app/app.component.ts
+++ resources/frontend/webcourses/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { AppService } from './app.service'
+import { SelectedCourseService } from './core/services/selected-course/selected-course.service'
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,12 @@ export class AppComponent implements OnInit {
 
   constructor(
     public appService: AppService,
+    public selectedCourseService: SelectedCourseService
   ) {}
 
   ngOnInit() {
     this.appService.initLogged()
+    this.selectedCourseService.servicePrimer()
   }
 
 }
