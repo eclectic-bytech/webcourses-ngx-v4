f83ffeb21fc94410926498027e79a6a44e7a371e
Sławomir Borkowski <slawomir@bytech.ca>
2021-09-23 19:14:33 -0500

    added servicePrimer method to selected-course-service

--- resources/frontend/webcourses/src/app/core/services/selected-course/selected-course.service.ts
+++ resources/frontend/webcourses/src/app/core/services/selected-course/selected-course.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core'
+import { WorkareaService } from 'src/app/pages/webcourse/activities/workarea/workarea.service'
+import { CourseService } from 'src/app/pages/catalogue/course/course.service'
+
+import { Course } from 'src/app/models/course.model'
+import { Chapter } from 'src/app/pages/webcourse/activities/models/chapter.model'
+import { Activity } from 'src/app/pages/webcourse/activities/workarea/models/activity.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SelectedCourseService {
 
-  constructor() { }
+  public selectedCourse: Course
+  public selectedChapter: Chapter
+
+  constructor(
+    private workareaService: WorkareaService,
+    private courseService: CourseService
+  ) { }
+
+  servicePrimer() {
+    this.workareaService.currentActivity$.subscribe(
+      (activity: Activity) => {
+        this.courseService.getCourse(activity.meta.course_id).subscribe(
+          (course: Course) => {
+            this.selectedCourse = course
+          }
+        )
+      }
+    )
+  }
+
 }
