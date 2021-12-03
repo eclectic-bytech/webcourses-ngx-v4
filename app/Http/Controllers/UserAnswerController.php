d3fbc926faf36cc36b829dedb4d4593dc6ab6329
Sławomir Borkowski <slawomir@bytech.ca>
2021-12-03 11:03:27 -0600

    now accessing user and user answer data in userAnswerController

--- app/Http/Controllers/UserAnswerController.php
+++ app/Http/Controllers/UserAnswerController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 
 class UserAnswerController extends Controller
 {
-    public function user_answer($aid) {
-        return $aid;
+    public function user_answer(Request $request, int $aid) {
+        $user = auth()->user();
+        $user_answers = $request->Input();
+        return 2100;
     }
 }
