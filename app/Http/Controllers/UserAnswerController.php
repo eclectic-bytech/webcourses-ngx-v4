233b6aca9cc48c0f7d408823780cc2282745e9cf
Sławomir Borkowski <slawomir@bytech.ca>
2021-12-06 23:22:32 -0600

    added and utilised user_answer method in userAnswer C

--- app/Http/Controllers/UserAnswerController.php
+++ app/Http/Controllers/UserAnswerController.php
@@ -9,20 +9,31 @@ class UserAnswerController extends Controller
 {
     public function save_user_answer(Request $request, int $aid) {
         // This should probably be turned into a HasCourseWriteAccess Guard...
+        // Maybe two: main, and second that checks for WriteAccess only.
         $uid = auth()->user()->id;
-        $cid = getActivityMeta($aid)->course_id; // global helper defined in app/Helpers/Course.php
         $pid = getUserProgress($uid, $cid)->id; // A pid confirms user is student in course
 
-        $existing_answer = false;
+        $activity_meta = getActivityMeta($aid)->course_id; // global helper defined in app/Helpers/Course.php
+        $cid = $activity_meta->course_id;
+        $activity_type = $activity_meta->activity_type;
 
         if ($pid) {
         // End Guard?
-            // Check if user already completed this activity
-            // get userAnswer where pid=x and where activity_id=y
-            $user_answers = $request->Input();
-            return $uid;
-        } else {
-            return 0;
+            $controller = new UserAnswerController();
+            $existing_answer = $controller->user_answer($pid, $aid);
+
+            if (!$existing_answer) {
+                $user_answers = $request->Input();
+                return 1;
+            }
         }
+        return 0;
+    }
+
+    public function user_answer(int $pid, int $aid) {
+        return UserAnswer
+            ::where('progress_id', $pid)
+            ->where('activity_id', $aid)
+            ->first();
     }
 }
