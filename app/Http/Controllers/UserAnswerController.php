e2f763ec58f105aade1f2e64cf262abc4b2cbe89
Sławomir Borkowski <slawomir@bytech.ca>
2021-12-07 04:58:53 -0600

    re-arranged order of code in userAnswer C

--- app/Http/Controllers/UserAnswerController.php
+++ app/Http/Controllers/UserAnswerController.php
@@ -10,14 +10,14 @@ class UserAnswerController extends Controller
     public function save_user_answer(Request $request, int $aid) {
         // This should probably be turned into a HasCourseWriteAccess Guard...
         // Maybe two: main, and second that checks for WriteAccess only.
-        $uid = auth()->user()->id;
-        $pid = getUserProgress($uid, $cid)->id; // A pid confirms user is student in course
+        $activity_meta = getActivityMeta($aid); // global helper defined in app/Helpers/Course.php
 
-        $activity_meta = getActivityMeta($aid)->course_id; // global helper defined in app/Helpers/Course.php
+        $uid = auth()->user()->id;
         $cid = $activity_meta->course_id;
+        $pid = getUserProgress($uid, $cid)->id; // A pid confirms user is student in course
         $activity_type = $activity_meta->activity_type;
 
-        if ($pid) {
+        if (isset($pid)) {
         // End Guard?
             $controller = new UserAnswerController();
             $existing_answer = $controller->user_answer($pid, $aid);
