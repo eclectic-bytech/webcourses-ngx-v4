0443a711f40557498edd4b777b30da8de6b6be5d
Sławomir B <starfox@bytech.ca>
2022-02-07 12:56:25 -0600

    squash commit of activity_save branch

--- app/Http/Controllers/UserAnswerController.php
+++ app/Http/Controllers/UserAnswerController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\UserAnswer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class UserAnswerController extends Controller
 {
+    // Currently returns autoincrement ID of inserted answer. Needs to return:
+    // { }
     public function save_user_answer(Request $request, int $aid) {
+        $uid = auth()->user()->id;
+
         // This should probably be turned into a HasCourseWriteAccess Guard...
         // Maybe two: courseReadAccess, and activityWriteAnswerAccess.
         $activity_meta = getActivityMeta($aid); // global helper defined in app/Helpers/Course.php
-
-        $uid = auth()->user()->id;
-        $cid = $activity_meta->course_id;
-        $pid = getUserProgress($uid, $cid)->id; // A pid confirms user is student in course
         $activity_type = $activity_meta->activity_type;
+        $cid = $activity_meta->course_id;
+        $chid = $activity_meta->chapter_id;
+
+        // Why do we need json_decode here, but not on $activity_meta?
+        $pid = json_decode(getUserProgress($uid, $cid))->id;
 
         if (isset($pid)) {
         // End Guard?
@@ -23,8 +30,24 @@ class UserAnswerController extends Controller
             $existing_answer = $controller->user_answer($pid, $aid);
 
             if (!$existing_answer) {
-                $user_answers = $request->Input();
-                return 1;
+                // If user has no answer for this activity, we continue...
+
+                // if ($activity_type === 'info') { }
+                // Do this using Eloquent?
+                // Remove Carbon:now() in fav of an auto DB solution?
+                // Or is this max compatibility, regardless of DB?
+                $answer_id = DB::table('user_answers')->insertGetId(
+                    array(
+                        'activity_id' => $aid,
+                        'chapter_id' => $chid,
+                        'progress_id' => $pid,
+                        'answer_id' => 42,
+                        'created_at' => Carbon::now()
+                    )
+                );
+                $answer['answers'] = array();
+                $answer['after_word'] = 'Em';
+                return $answer;
             }
         }
         return 0;
@@ -35,5 +58,15 @@ class UserAnswerController extends Controller
             ::where('progress_id', $pid)
             ->where('activity_id', $aid)
             ->first();
+    }
+
+    // public function total_user_answers_in_chapter(int $chid) {
+    public function total_user_answers_in_chapter(int $chid) {
+        $answers = UserAnswer
+            ::where('progress_id', 1)
+            ->where('chapter_id', 309)
+            // ->groupBy('activity_id')
+            ->get();
+        return $answers->count();
     }
 }
