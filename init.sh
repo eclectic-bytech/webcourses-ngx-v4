d038734a8acd0952d1ae6817f6526a0da441beaf
Collier Lee <collier@bytech.ca>
2022-04-09 14:01:19 -0500

    default user seeder working

--- init.sh
+++ init.sh
@@ -3,3 +3,9 @@ php ~/code/artisan migrate;
 
 echo "Copying Angular index.html to angular.blade.php....";
 cp ~/code/public/webcourses/index.html ~/code/resources/views/angular.blade.php;
+
+echo "Creating default team for developers.";
+php artisan db:seed --class=TeamSeeder
+
+echo "Creating default user for developers.";
+php artisan db:seed --class=UserSeeder
