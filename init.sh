88f300eca2d56b54052f0af115cfe639920338a4
Collier Lee <collier@bytech.ca>
2022-05-05 12:28:38 -0500

    Updated init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,10 @@ echo "Copying Angular index.html to angular.blade.php....";
 cp ~/code/public/webcourses/index.html ~/code/resources/views/angular.blade.php;
 
 echo "Seeding default user team...";
-php ~/code/artisan db:seed --class=TeamSeeder
+php ~/code/artisan db:seed --class=TeamSeeder;
 
 echo "Seeding default admin user...";
-php ~/code/artisan db:seed --class=UserSeeder
+php ~/code/artisan db:seed --class=UserSeeder;
+
+echo "Seeding default course...";
+php ~/code/artisan db:seed --class=DemocourseSeeder;
