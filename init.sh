3393b74820f4d2ef70d0f3e2cc3257c13ecbe3d7
Sławomir Borkowski <slawomir@bytech.ca>
2022-06-25 19:23:37 -0500

    installer fixes

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 echo "Laravel migration is starting...";
 php ~/code/artisan migrate;
-
+echo
 echo "Copying Angular index.html to angular.blade.php....";
 cp ~/code/public/webcourses/index.html ~/code/resources/views/angular.blade.php;
-
+echo
 echo "Seeding default user team...";
 php ~/code/artisan db:seed --class=TeamSeeder;
-
+echo
 echo "Seeding default admin user...";
 php ~/code/artisan db:seed --class=UserSeeder;
-
+echo
 echo "Seeding default course...";
 php ~/code/artisan db:seed --class=DemocourseSeeder;
