9f307896240ddfb5388faa1cbbfc13644e370135
Sławomir Borkowski <slawomir@bytech.ca>
2022-09-27 11:28:53 +0200

    removed hard coded default sign-in icon, now using gravatar default - for now

--- resources/frontend/webcourses/src/app/core/services/gravatar-md5/gravatar-md5.service.ts
+++ resources/frontend/webcourses/src/app/core/services/gravatar-md5/gravatar-md5.service.ts
@@ -6,13 +6,17 @@ import { Md5 } from 'ts-md5/dist/md5'
 })
 export class GravatarMd5Service {
 
-  private defaultAvatar = encodeURI('https://www.cultivatelearning.ca/assets/avatar.png')
+  //     Used for default avatar when user is not logged in.
+  //     Requires full https path. We can use it again once URL is available from config
+  // private defaultAvatar = encodeURI('https://www.cultivatelearning.ca/assets/avatar.png')
 
   constructor() { }
 
   gravatarMD5src(email) {
     const md5email = Md5.hashStr(email)
-    const gravatarSrc = 'https://www.gravatar.com/avatar/' + md5email + '?d=' + this.defaultAvatar
+    const gravatarSrc = 'https://www.gravatar.com/avatar/' + md5email
+    // See note above definition for defaultAvatar
+    // const gravatarSrc = 'https://www.gravatar.com/avatar/' + md5email + '?d=' + this.defaultAvatar
     return gravatarSrc
   }
 
