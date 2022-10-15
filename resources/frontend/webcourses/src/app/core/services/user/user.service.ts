a6906f8942800c761fa317d40173daef6af322ad
Sławomir Borkowski <slawomir@bytech.ca>
2022-10-15 11:45:32 +0200

    replace user services's behaviourSubject with subject and updated header bar

--- resources/frontend/webcourses/src/app/core/services/user/user.service.ts
+++ resources/frontend/webcourses/src/app/core/services/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { BehaviorSubject, Observable } from 'rxjs'
+import { Subject, Observable } from 'rxjs'
 import { shareReplay } from 'rxjs/operators'
 
 import { JetstreamUser } from '../../models/jetstream-user.model'
@@ -14,53 +14,7 @@ export class UserService {
 
   userCourses$: Observable<Course[]> = this.getUserCourses$()
 
-  user$ = new BehaviorSubject<JetstreamUser>(<JetstreamUser>{
-		id: 1,
-		name: "Guest",
-		first_name: null,
-		last_name: null,
-		email: null,
-		email_verified_at: null,
-		current_team_id: 1,
-		profile_photo_path: null,
-		created_at: null,
-		updated_at: null,
-		profile_photo_url: null,
-		current_team: {
-			id: 1,
-			user_id: 1,
-			name: "Administrators",
-			personal_team: true,
-			created_at: null,
-			updated_at: null
-		},
-		all_teams: [
-			{
-				id: 1,
-				user_id: 1,
-				name: "Administrators",
-				personal_team: true,
-				created_at: null,
-				updated_at: null,
-				membership: {
-					user_id: 1,
-					team_id: 1,
-					role: "Admin role.",
-					created_at: null,
-					updated_at: null
-				}
-			},
-			{
-				id: 2,
-				user_id: 1,
-				name: "Publishers",
-				personal_team: false,
-				created_at: null,
-				updated_at: null
-			}
-		],
-		two_factor_enabled: false
-  })
+  user$ = new Subject<JetstreamUser>()
 
   constructor(
     private httpClient: HttpClient,
