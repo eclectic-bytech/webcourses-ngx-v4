20c105ce24cb1b001f2139ce50a2f0c0fdf00add
Sławomir Borkowski <slawomir@bytech.ca>
2022-11-07 09:40:48 +0100

    created and implemented after.sh in package.json, removed ln commands from installer

--- installer.sh
+++ installer.sh
@@ -1,27 +1,23 @@
-echo -e "\033[1;35mWNGX Installer v1.1.0a\033[1;34m";
+# echo -e "\033[1;35mWNGX Installer v1.1.0a\033[1;34m";
 
-echo -e "Running Composer...";
-composer install;
+# echo -e "Running Composer...";
+# composer install;
 
-echo -e "Creating a copy of .env file...";
-cp .env.example .env;
+# echo -e "Creating a copy of .env file...";
+# cp .env.example .env;
 
-echo -e "Installing NPM packages needed by Vue...";
-npm install;
+# echo -e "Installing NPM packages needed by Vue...";
+# npm install;
 
 echo -e "Installing NPM packages needed by Angular...";
 cd resources/webcourses/;
-npm install;
+# npm install;
 
-echo
-echo -e "Compiling webcourses (Angular framework)...";
-npm run build:prod;
+# echo
+# echo -e "Compiling webcourses (Angular framework)...";
+# npm run build:prod;
 
-cd ../../public/webcourses/publisher-files/
-echo -e "Creating symbolic link associations in publisher-files directory...";
-ln -s 1 webcourses_io;
-ln -s 2 acme;
-cd ../../../;
+cd ../../
 
 echo
 echo "Running homestead make...";
