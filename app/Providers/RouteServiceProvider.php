5bf20bbd7427330dcb690763855c7f432ae36f15
Sławomir Borkowski <slawomir@bytech.ca>
2022-11-07 14:14:05 +0100

    v4/user/redirect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/user/redirect';
+    public const HOME = '/v4/user/redirect';
 
     /**
      * The controller namespace for the application.
