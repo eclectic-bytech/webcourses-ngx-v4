7c7ac20fdab855193b34065ab2f1cc172e3b7ba9
Sławomir Borkowski <slawomir@bytech.ca>
2022-11-16 14:00:51 +0100

    updated angular user model to match jetstream user

--- resources/webcourses/src/app/core/models/jetstream-user.model.ts
+++ resources/webcourses/src/app/core/models/jetstream-user.model.ts
@@ -5,34 +5,10 @@ export class JetstreamUser {
   last_name: string
   email: string
   email_verified_at: string
+  current_course_id: number
   current_team_id: number
   profile_photo_path: string
   created_at: string
   updated_at: string
   profile_photo_url: string
-  current_team: {
-    id: number
-    user_id: number
-    name: string
-    personal_team: boolean
-    created_at: string
-    updated_at: string
-  }
-  all_teams:
-    {
-      id: number
-      user_id: number
-      name: string
-      personal_team: boolean
-      created_at: string
-      updated_at: string
-      membership?: {
-        user_id: number
-        team_id: number
-        role: string
-        created_at: string
-        updated_at: string
-      }
-    }[]
-  two_factor_enabled: boolean
 }
