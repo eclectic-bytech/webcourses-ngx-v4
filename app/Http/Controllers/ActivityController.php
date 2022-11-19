5620fa8a8f6e25cc0e40f480059a572d1abddf4f
Sławomir Borkowski <slawomir@bytech.ca>
2022-11-19 01:14:47 +0100

    a good start

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -11,8 +11,20 @@ class ActivityController extends Controller
     public function activity($aid = false) {
         $activity = Activity
             ::where('id', $aid)
-            ->with('meta', 'answers')
+            ->with('meta', 'answers', 'user_answers')
             ->get();
+
+        if (!count($activity[0]['user_answers'])) {
+            unset($activity[0]['user_answers'], $activity[0]['after_word']);
+        } else {
+            foreach ($activity[0]['answers'] as $key => $answer) {
+                foreach ($activity[0]['user_answers'] as $key2 => $user_answer) {
+                    if ($answer['id'] === $user_answer['answer_id']) {
+                        $activity[0]['answers'][$key]['selected'] = true;
+                    }
+                }
+            }
+        }
 
         // Answer caption for Click type activity where meta.style is image need json_decode
         if ($activity[0]['meta']['activity_type'] === "click" && $activity[0]['meta']['style'] === "image") {
