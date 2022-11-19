180d812fbf9d886271f1573981e3271099fbeb9a
Sławomir Borkowski <slawomir@bytech.ca>
2022-11-19 15:57:02 +0100

    fetching activity if user has access and it's first in set

--- app/Http/Controllers/SyllabusController.php
+++ app/Http/Controllers/SyllabusController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\ActivityController;
 use App\Models\Syllabus;
 
 class SyllabusController extends Controller
@@ -12,7 +13,27 @@ class SyllabusController extends Controller
 
         $requested_activity_meta = Syllabus::where('activity_id', $aid)->first();
 
-        return $requested_activity_meta;
+        $uid = auth()->user()->id;
+        $pid = getUserProgress($uid, $requested_activity_meta->course_id)->id;
+
+        if ($pid) {
+            // User has access to course
+
+            if ($requested_activity_meta->cont === 0) {
+                // requested activity is first in set
+                $controller = new ActivityController();
+                $activity[0] = $controller->activity($aid);
+                return $activity;
+
+            } else {
+                // requested activity is not the first in set
+
+            }
+
+            return $requested_activity_meta;
+        }
+
+        return 'No soup for you';
 
     }
 }
