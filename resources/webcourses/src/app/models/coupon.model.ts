6f1d3c222acd686e200fa3624cab17ebf852fda2
Sławomir Borkowski <slawomir@bytech.ca>
2022-12-04 10:59:23 +0100

    coupon model

---
+++ resources/webcourses/src/app/models/coupon.model.ts
@@ -0,0 +1,14 @@
+export class Coupon {
+  id: string
+  code: string
+  label: string
+  active: boolean
+  discount: number
+  cid: number
+  gid: number
+  expiry: string
+  uses: number
+  uses_max: number
+  created_at: string
+  updated_at: string
+}
