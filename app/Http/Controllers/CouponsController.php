727fbe603e72f0ec6a14d4814c9c8edbe92f00b3
Sławomir Borkowski <slawomir@bytech.ca>
2022-12-04 13:56:50 +0100

    fetches all coupons, needs filtering for those owned by publisher

--- app/Http/Controllers/CouponsController.php
+++ app/Http/Controllers/CouponsController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Coupons;
 
 class CouponsController extends Controller
 {
-    //
+    public function index() {
+        return Coupons::get();
+    }
 }
