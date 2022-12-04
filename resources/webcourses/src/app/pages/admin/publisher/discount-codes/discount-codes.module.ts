e588db4318d8701aa51d9e3b73840daaea523d6e
Sławomir Borkowski <slawomir@bytech.ca>
2022-12-04 12:57:56 +0100

    added loading spinner to codes data

--- resources/webcourses/src/app/pages/admin/publisher/discount-codes/discount-codes.module.ts
+++ resources/webcourses/src/app/pages/admin/publisher/discount-codes/discount-codes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { DiscountCodesComponent } from './discount-codes.component'
 import { DiscountCodesRoutingModule } from './discount-codes-routing.module'
+import { LoadingSpinnerModule } from 'src/app/core/modules/loading-spinner/loading-spinner.module'
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { DiscountCodesRoutingModule } from './discount-codes-routing.module'
   ],
   imports: [
     CommonModule,
-    DiscountCodesRoutingModule
+    DiscountCodesRoutingModule,
+    LoadingSpinnerModule
   ]
 })
 
