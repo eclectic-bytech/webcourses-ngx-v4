c06d0244eb985dd4e7cf06d78d37326aaaec9fd7
Sławomir Borkowski <slawomir@bytech.ca>
2022-12-13 15:11:38 +0100

    cleaned up import paths

--- resources/webcourses/src/app/pages/webcourse/activities/sidebar/selected/selected.component.ts
+++ resources/webcourses/src/app/pages/webcourse/activities/sidebar/selected/selected.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core'
 import { SelectedService } from './selected.service'
-import { CompletionStatsService } from '../../../../../core/services/user/completion-stats.service'
-import { FadeInOut } from '../../../../../core/animations/fade-in-out.animation'
-import { SelectedCourseService } from '../../../../../../../src/app/core/services/selected-course/selected-course.service'
+import { CompletionStatsService } from 'src/app/core/services/user/completion-stats.service'
+import { FadeInOut } from 'src/app/core/animations/fade-in-out.animation'
+import { SelectedCourseService } from 'src/app/core/services/selected-course/selected-course.service'
 
 @Component({
   selector: 'app-selected',
