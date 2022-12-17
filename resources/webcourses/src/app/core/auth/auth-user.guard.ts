894c4d48ee0743f28eaee5c747deeadae91ec4b1
Sławomir Borkowski <slawomir@bytech.ca>
2022-12-17 12:20:19 +0100

    removed router from auth-user guard

--- resources/webcourses/src/app/core/auth/auth-user.guard.ts
+++ resources/webcourses/src/app/core/auth/auth-user.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { CanActivate, Router, UrlTree } from '@angular/router'
+import { CanActivate, UrlTree } from '@angular/router'
 import { Observable } from 'rxjs'
 import { UserService } from 'src/app/core/services/user/user.service'
 
@@ -10,14 +10,12 @@ import { UserService } from 'src/app/core/services/user/user.service'
 export class AuthUserGuard implements CanActivate {
 
   constructor(
-    private router: Router,
     private userService: UserService
   ) {}
 
   canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
   {
-    console.log('AuthGuard functioning!')
-    return true
+    return this.userService.isLoggedIn$
   }
 
 }
