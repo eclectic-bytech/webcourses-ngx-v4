4d79d296d3dbea746473ac6a613b227482d9a50e
Sławomir Borkowski <slawomir@bytech.ca>
2023-01-03 15:46:18 +0100

    workarea activity help tab improvements

--- resources/webcourses/src/app/pages/webcourse/activities/sidebar/activity-help/activity-help.component.ts
+++ resources/webcourses/src/app/pages/webcourse/activities/sidebar/activity-help/activity-help.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core'
-import { WorkareaService } from './../../workarea/workarea.service'
 import { HttpClient } from '@angular/common/http'
+import { BehaviorSubject } from 'rxjs'
+import { shareReplay } from 'rxjs/operators'
+
+// WNGX Services
+import { ConfigService } from '../../../../../core/services/config/config.service'
+import { SelectedCourseService } from 'src/app/core/services/selected-course/selected-course.service'
+
+// WNGX Models and Misc
 import { Activity } from '../../workarea/models/activity.model'
 import { FadeInOut } from '../../../../../core/animations/fade-in-out.animation'
-import { ConfigService } from '../../../../../core/services/config/config.service'
 
 @Component({
   selector: 'app-activity-help',
@@ -13,38 +19,28 @@ import { ConfigService } from '../../../../../core/services/config/config.servic
 })
 export class ActivityHelpComponent implements OnInit {
 
-  activityHelp: string[] = new Array()
-  activityType: string
+  public activityHelp$ = new BehaviorSubject<string | null>(null)
 
   constructor(
     private httpClient: HttpClient,
     private configService: ConfigService,
-    public workareaService: WorkareaService,
-  ) {
-    this.activityType = this.workareaService.activities[this.workareaService.activities.length - 1].meta.activity_type
-  }
+    private selectedCourseService: SelectedCourseService
+  ) { }
 
   ngOnInit() {
-    this.getActivityHelp(this.activityType)
-
-    this.workareaService.currentActivitySet$
-      .subscribe(
-        (activities: Activity[]) => {
-          this.activityType = activities[0].meta.activity_type
-          this.getActivityHelp(this.activityType) },
-        (err) => { console.log(err) }
-      )
+    this.selectedCourseService.selectedActivitySet$.subscribe(
+      (activities: Activity[]) => {
+        this.getActivityHelp(activities[activities.length - 1].meta.activity_type).subscribe(
+          (help: string) => { this.activityHelp$.next(help) }
+        )
+      }
+    )
   }
 
   getActivityHelp(type: string) {
-    if (!this.activityHelp[type]) {
-      this.httpClient.get<string>(
-        `${this.configService.params.api.route}/webcourse/activities/help/${type}`
-      ).subscribe(
-        (help) => { this.activityHelp[type] = help },
-        (err) => { console.log(err) }
-      )
-    }
+    return this.httpClient.get<string>(
+      `${this.configService.params.api.route}/webcourse/activities/help/${type}`
+    ).pipe(shareReplay(1))
   }
 
 }
