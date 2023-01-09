44875a60ed2d32b5ed081aa09ca3f16b45ae2798
Sławomir Borkowski <slawomir@bytech.ca>
2023-01-09 18:02:15 +0100

    refactoring of enf of chapter component and template

--- resources/webcourses/src/app/pages/webcourse/activities/workarea/components/chapter-end/chapter-end.component.ts
+++ resources/webcourses/src/app/pages/webcourse/activities/workarea/components/chapter-end/chapter-end.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core'
-import { Location } from '@angular/common'
 
 // WNGX services
 import { SelectedCourseService } from 'src/app/core/services/selected-course/selected-course.service'
+import { NavService } from '../../nav/nav.service'
 
 // WNGX models and misc
 import { FadeInOut } from '../../../../../../core/animations/fade-in-out.animation'
-import { Chapter } from '../../../models/chapter.model'
-import { NavService } from '../../nav/nav.service'
 
 @Component({
   selector: 'app-chapter-end',
@@ -18,18 +16,8 @@ import { NavService } from '../../nav/nav.service'
 export class ChapterEndComponent {
 
   constructor(
-    private location: Location,
     public selectedCourseService: SelectedCourseService,
-    private navService: NavService
+    public navService: NavService
   ) { }
-
-  continueButton(nextChapter: Chapter) {
-    if (nextChapter) {
-      this.location.go(`/webcourse/activities/${nextChapter.syllabus[0].activity_id}`)
-    } else {
-      this.navService.endOfChapter = false
-      this.navService.endOfCourse = true
-    }
-  }
 
 }
