f452a964f97afeb59896cf99cd7fb5dec71762f9
Sławomir Borkowski <slawomir@bytech.ca>
2023-01-18 20:26:11 +0100

    added course relationship to coupon

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Coupon extends Model
 {
     use HasFactory;
+
+    public function course() {
+        return $this->hasOne(Course::class, 'id', 'cid');
+    }
 }
