fc460fe66295c7d1bff8ee6a80bc43cb4f45ccb0
Sławomir Borkowski <slawomir@bytech.ca>
2023-01-26 19:52:56 +0100

    fetch user pub_id in courseEditorController

--- app/Http/Controllers/CourseEditorController.php
+++ app/Http/Controllers/CourseEditorController.php
@@ -4,9 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Publisher;
+
 class CourseEditorController extends Controller
 {
     public function demo($aid) {
-        return json_encode("Yay!");
+        $uid = auth()->user()->id;
+
+        // get user's publisher id
+        $pub_id = Publisher::where('owner_uid', $uid)->first()->id;
+
+        // if ($pub_id) {
+
+        // }
+
+        return json_encode($pub_id);
     }
 }
