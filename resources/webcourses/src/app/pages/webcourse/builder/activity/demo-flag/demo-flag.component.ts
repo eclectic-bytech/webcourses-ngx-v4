adbe59c75878be35f902c03ef24ced41ac817f41
Sławomir Borkowski <slawomir@bytech.ca>
2023-01-26 17:31:58 +0100

    ui for make activity available in demo

--- resources/webcourses/src/app/pages/webcourse/builder/activity/demo-flag/demo-flag.component.ts
+++ resources/webcourses/src/app/pages/webcourse/builder/activity/demo-flag/demo-flag.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivityMeta } from '../../../activities/workarea/models/activity-meta.model';
 
 @Component({
   selector: 'app-demo-flag',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo-flag.component.sass']
 })
 export class DemoFlagComponent implements OnInit {
+
+  @Input() activityMeta: ActivityMeta
 
   constructor() { }
 
--- resources/webcourses/src/app/pages/webcourse/builder/activity/demo-flag/demo-flag.module.ts
+++ resources/webcourses/src/app/pages/webcourse/builder/activity/demo-flag/demo-flag.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemoFlagComponent } from './demo-flag.component';
-
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     DemoFlagComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [ DemoFlagComponent ]
 })
