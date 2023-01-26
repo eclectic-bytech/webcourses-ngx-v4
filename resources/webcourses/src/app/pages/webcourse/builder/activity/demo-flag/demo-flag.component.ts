4a24bc40726ca4829349fb232f8d0e75b8e1f3d0
Sławomir Borkowski <slawomir@bytech.ca>
2023-01-26 14:06:57 +0100

    front-end demo flag MCS boiler plate

---
+++ resources/webcourses/src/app/pages/webcourse/builder/activity/demo-flag/demo-flag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-flag',
+  templateUrl: './demo-flag.component.html',
+  styleUrls: ['./demo-flag.component.sass']
+})
+export class DemoFlagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ resources/webcourses/src/app/pages/webcourse/builder/activity/demo-flag/demo-flag.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DemoFlagComponent } from './demo-flag.component';
+
+
+
+@NgModule({
+  declarations: [
+    DemoFlagComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DemoFlagModule { }
