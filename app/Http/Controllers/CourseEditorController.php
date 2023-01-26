b531c17ea24e539ef4c9c024696189fe4699c57e
Sławomir Borkowski <slawomir@bytech.ca>
2023-01-26 20:47:01 +0100

    functioning demo course flag update

--- app/Http/Controllers/CourseEditorController.php
+++ app/Http/Controllers/CourseEditorController.php
@@ -3,21 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\Publisher;
+use App\Models\Course;
+use App\Models\CourseSyllabus;
 
 class CourseEditorController extends Controller
 {
-    public function demo($aid) {
+    public function demo(int $aid) {
         $uid = auth()->user()->id;
 
         // get user's publisher id
-        $pub_id = Publisher::where('owner_uid', $uid)->first()->id;
+        $uid_pub_id = Publisher::where('owner_uid', $uid)->first()->id;
 
-        // if ($pub_id) {
+        if ($uid_pub_id) {
 
-        // }
+            // get activity's publisher id
+            $activity_cid = CourseSyllabus::where('activity_id', $aid)->first()->course_id;
+            $cid_pub_id = Course::firstWhere('id', $activity_cid)->publisher_id;
 
-        return json_encode($pub_id);
+            // if user publisher id matches activity's pub id, update db
+            if ($uid_pub_id === $cid_pub_id) {
+                DB::table('course_syllabus')->where('activity_id', $aid)->update(['demo' => DB::raw('NOT demo')]);
+            }
+        }
+
+        return false;
     }
 }
