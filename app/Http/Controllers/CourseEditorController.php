96acc5b4ca33126938acd7ae048ca8bb84c81754
Sławomir Borkowski <slawomir@bytech.ca>
2023-01-26 20:59:17 +0100

    prevent error if user is not publisher

--- app/Http/Controllers/CourseEditorController.php
+++ app/Http/Controllers/CourseEditorController.php
@@ -15,7 +15,7 @@ class CourseEditorController extends Controller
         $uid = auth()->user()->id;
 
         // get user's publisher id
-        $uid_pub_id = Publisher::where('owner_uid', $uid)->first()->id;
+        $uid_pub_id = Publisher::where('owner_uid', $uid)->first();
 
         if ($uid_pub_id) {
 
@@ -24,7 +24,7 @@ class CourseEditorController extends Controller
             $cid_pub_id = Course::firstWhere('id', $activity_cid)->publisher_id;
 
             // if user publisher id matches activity's pub id, update db
-            if ($uid_pub_id === $cid_pub_id) {
+            if ($uid_pub_id->id === $cid_pub_id) {
                 DB::table('course_syllabus')->where('activity_id', $aid)->update(['demo' => DB::raw('NOT demo')]);
             }
         }
