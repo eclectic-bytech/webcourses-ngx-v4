56624378807444f740d8c2afbba51cb294c68689
Sławomir Borkowski <slawomir@bytech.ca>
2023-01-30 20:06:13 +0100

    access code form

--- resources/webcourses/src/app/pages/components/access-code-modal/access-code-modal.component.ts
+++ resources/webcourses/src/app/pages/components/access-code-modal/access-code-modal.component.ts
@@ -2,21 +2,27 @@ import { Component } from '@angular/core'
 import { AccessCodeModalService } from './access-code-modal.service'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { faFingerprint } from '@fortawesome/free-solid-svg-icons'
+import { FormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-access-code-modal',
   templateUrl: './access-code-modal.component.html',
   styleUrls: ['./access-code-modal.component.sass'],
   standalone: true,
-  imports: [FontAwesomeModule]
+  imports: [FontAwesomeModule, FormsModule, CommonModule]
 })
 
 export class AccessCodeModalComponent {
-
+  public accessCode: string
   public faFingerprint = faFingerprint
 
   constructor(
     public accessCodeModalService: AccessCodeModalService
   ) { }
+
+  submitCodeButton(accessCode: string) {
+    this.accessCodeModalService.submitCode(accessCode)
+  }
 
 }
--- resources/webcourses/src/app/pages/components/access-code-modal/access-code-modal.service.ts
+++ resources/webcourses/src/app/pages/components/access-code-modal/access-code-modal.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 import { Course } from 'src/app/models/course.model'
+import { ConfigService } from 'src/app/core/services/config/config.service'
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AccessCodeModalService {
+
   public course: Course
-  constructor() { }
+
+  constructor(
+    private httpClient: HttpClient,
+    private configService: ConfigService
+  ) { }
+
+  submitCode(code:string) {
+    this.httpClient.post(`${this.configService.params.api.route}/k`, code).subscribe(
+      (data) => {
+        console.log("It's alive!")
+        console.log(data)
+      }
+    )
+  }
+
 }
