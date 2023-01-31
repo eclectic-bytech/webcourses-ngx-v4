c412a426adf1ec508466f3a1f0e5bd22b12907d6
Sławomir Borkowski <slawomir@bytech.ca>
2023-01-31 18:07:39 +0100

    custom fade for value update

--- resources/webcourses/src/app/pages/components/access-code-modal/access-code-modal.component.ts
+++ resources/webcourses/src/app/pages/components/access-code-modal/access-code-modal.component.ts
@@ -4,19 +4,28 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { faFingerprint, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common'
-import { FadeInOut } from 'src/app/core/animations/fade-in-out.animation'
+import { trigger, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-access-code-modal',
   templateUrl: './access-code-modal.component.html',
   styleUrls: ['./access-code-modal.component.sass'],
   standalone: true,
-  animations: [FadeInOut],
-  imports: [FontAwesomeModule, FormsModule, CommonModule]
+  imports: [FontAwesomeModule, FormsModule, CommonModule],
+  animations: [
+    trigger('valueAnimation', [
+      transition('* => *', [
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 
 export class AccessCodeModalComponent {
+
   public accessCode: string
+
   public faFingerprint = faFingerprint
   public faSpinner = faSpinner
 
@@ -25,6 +34,7 @@ export class AccessCodeModalComponent {
   ) { }
 
   get course(){ return this.accessCodeModalService.course }
+  get fromStatus(){ return this.accessCodeModalService.formStatus }
 
   submitCodeButton(accessCode: string) {
     let cid: number = this.course ? this.course.id : 0
