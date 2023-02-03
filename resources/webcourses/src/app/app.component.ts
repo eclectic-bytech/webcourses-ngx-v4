388b4dd2551b4bc1e5042007ab1fad6c1168ed98
SirL3X0N <seth@bytech.ca>
2023-02-03 13:54:13 +0100

    Added function to display warning message

--- resources/webcourses/src/app/app.component.ts
+++ resources/webcourses/src/app/app.component.ts
@@ -25,10 +25,11 @@ export class AppComponent implements OnInit, OnDestroy {
     this.sub = this.selectedCourseService.selectedCourse$.subscribe(
       (course: Course) => { if (course) this.themeService.changeTheme(course.theme) }
     )
-
-
+this.webDeveloperToolsWarning()
   }
-
+  webDeveloperToolsWarning(){
+    console.warn ('\r\n\r\n .d8888b. 88888888888 .d88888b.  8888888b.       888 \r\nd88P  Y88b   \"888\"   d88P\" \"Y88b 888   Y88b      888         This tool is intended for developer use\r\nY88b.         888    888     888 888    888      888         only. If anyone told you to copy-paste\r\n \"Y888b.      888    888     888 888   d88P      888         anything in here it is most likely a scam.\r\n    \"Y88b.    888    888     888 8888888P\"       888         You could loose your account or reveal \r\n      \"888    888    888     888 888             Y8P         personal data to unwelcomed eyes!\r\nY88b  d88P    888    Y88b. .d88P 888              \"  \r\n \"Y8888P\"     888     \"Y88888P\"  888             888 \r\n                                                     \r\n                                                     \r\n                                                     \r\n                                            \r\n                                            ')
+  }
   ngOnDestroy() {
     this.sub.unsubscribe()
   }
