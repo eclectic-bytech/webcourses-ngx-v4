0ba46a986df4a13d5b7fb92fa15210f888c31bb5
Sławomir Borkowski <slawomir@bytech.ca>
2023-02-07 22:55:42 +0100

    user guard updates

--- resources/webcourses/src/app/core/auth/auth-user.guard.ts
+++ resources/webcourses/src/app/core/auth/auth-user.guard.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core'
 import { CanActivate, UrlTree } from '@angular/router'
 import { Observable } from 'rxjs'
 import { UserService } from 'src/app/core/services/user/user.service'
-
+import { SessionExpiredService } from '../modals/session-expired/session-expired.service'
+import { map } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
@@ -10,12 +11,26 @@ import { UserService } from 'src/app/core/services/user/user.service'
 export class AuthUserGuard implements CanActivate {
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private sessionExpiredService: SessionExpiredService
   ) {}
 
   canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
   {
-    return this.userService.isLoggedIn$
+
+    return this.userService.getUser().pipe(map(
+      (user) => {
+        this.userService.user = user
+        if (user) {
+          return true
+        } else {
+          this.sessionExpiredService.sessionExpiredModal(500)
+        }
+      },
+      (err) => {
+        this.sessionExpiredService.sessionExpiredModal(err['status'])
+      }
+    ))
   }
 
 }
