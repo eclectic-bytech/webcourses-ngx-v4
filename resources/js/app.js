8d3fd0dfd73dd8b5a4c143e53d3e36b88111f65a
SirL3X0N <seth@bytech.ca>
2023-02-22 13:47:57 +0100

    did font awesome for vue step one in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,18 @@ import { createApp, h } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin, Link } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
 const el = document.getElementById('app');
 
 createApp({
@@ -16,7 +28,7 @@ createApp({
 })
     .mixin({ methods: { route } })
     .use(InertiaPlugin)
-    .component('inertia-link', Link)
-    .mount(el);
+    .component('inertia-link', Link, 'font-awesome-icon', FontAwesomeIcon)
+    .mount(app, el);
 
 InertiaProgress.init({ color: '#4B5563' });
