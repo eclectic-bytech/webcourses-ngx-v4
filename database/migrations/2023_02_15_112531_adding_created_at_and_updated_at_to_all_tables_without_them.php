8bb139126e51642a27f69cdfc52fdde40610ef75
Sławomir Borkowski <slawomir@bytech.ca>
2023-02-23 11:52:00 +0100

    removed created_at and updated_at from laravel table migrations #176

--- database/migrations/2023_02_15_112531_adding_created_at_and_updated_at_to_all_tables_without_them.php
+++ database/migrations/2023_02_15_112531_adding_created_at_and_updated_at_to_all_tables_without_them.php
@@ -25,23 +25,8 @@ return new class extends Migration
             $table->timestamp('created_at')->nullable()->default(NULL)->after('help');
             $table->timestamp('updated_at')->nullable()->default(NULL)->after('created_at');
         });
-        Schema::table('failed_jobs', function (Blueprint $table) {
-            $table->timestamp('created_at')->nullable()->default(NULL)->after('failed_at');
-            $table->timestamp('updated_at')->nullable()->default(NULL)->after('created_at');
-        });
-        Schema::table('migrations', function (Blueprint $table) {
-            $table->timestamp('created_at')->nullable()->default(NULL)->after('batch');
-            $table->timestamp('updated_at')->nullable()->default(NULL)->after('created_at');
-        });
-        Schema::table('password_resets', function (Blueprint $table) {
-            $table->timestamp('updated_at')->nullable()->default(NULL)->after('created_at');
-        });
         Schema::table('roles', function (Blueprint $table) {
             $table->timestamp('created_at')->nullable()->default(NULL)->after('title');
-            $table->timestamp('updated_at')->nullable()->default(NULL)->after('created_at');
-        });
-        Schema::table('sessions', function (Blueprint $table) {
-            $table->timestamp('created_at')->nullable()->default(NULL)->after('last_activity');
             $table->timestamp('updated_at')->nullable()->default(NULL)->after('created_at');
         });
         Schema::table('user_long_answers', function (Blueprint $table) {
