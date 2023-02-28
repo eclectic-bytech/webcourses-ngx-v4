e267ccbd0637873e7d044efe019015c140ba2ef6
Sławomir Borkowski <slawomir@bytech.ca>
2023-02-28 17:22:09 +0100

    Squashed commit of the following:
    commit c38d9ebe0f9e1833873513e06bd355145e8d9096
    Author: Sławomir Borkowski <slawomir@bytech.ca>
    Date:   Tue Feb 28 17:16:33 2023 +0100
    
        code formatting update
    
    commit fabf4b2989669577d1c1ce99636360eb71b2f583
    Author: Sławomir Borkowski <slawomir@bytech.ca>
    Date:   Tue Feb 28 17:16:18 2023 +0100
    
        documentation
    
    commit d8f401f287a5e8b98ca3c1eeb97f89afd0b8eb8a
    Author: Sławomir Borkowski <slawomir@bytech.ca>
    Date:   Tue Feb 28 17:12:44 2023 +0100
    
        changed col order in ip-logging issue migration
    
    commit db8c344ee2a158824fcc1ce4c3eefe9688566878
    Author: SirL3X0N <seth@bytech.ca>
    Date:   Tue Feb 28 12:15:49 2023 +0100
    
        Got the db changes to work
    
    commit 820f25b19fd2ca701a52487ea93aba8b1401c536
    Author: SirL3X0N <seth@bytech.ca>
    Date:   Mon Feb 27 14:01:44 2023 +0100
    
        so far

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,10 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Auth\Events\Login;
+
+// WNGX imports
+use App\Listeners\LoginListener;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +21,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        Login::class => [
+            LoginListener::class,
         ],
     ];
 
