4a148b487498b02288fb39d4ddfdfd43773f7935
Sławomir Borkowski <slawomir@bytech.ca>
2023-03-03 19:35:51 +0100

    discount-codes-users boilerplate in angular

---
+++ resources/webcourses/src/app/pages/admin/publisher/discount-codes/discount-codes-users/discount-codes-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discount-codes-users',
+  templateUrl: './discount-codes-users.component.html',
+  styleUrls: ['./discount-codes-users.component.sass']
+})
+export class DiscountCodesUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
