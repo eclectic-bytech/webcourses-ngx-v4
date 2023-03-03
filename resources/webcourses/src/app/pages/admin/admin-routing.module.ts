c7a3adb879eb05d578c2b8ea3f59f4c2c9c758e9
Sławomir Borkowski <slawomir@bytech.ca>
2023-03-03 19:35:21 +0100

    removed pathmatchfull from admin-routing

--- resources/webcourses/src/app/pages/admin/admin-routing.module.ts
+++ resources/webcourses/src/app/pages/admin/admin-routing.module.ts
@@ -5,18 +5,15 @@ const routes: Routes = [
   {
     path: 'publisher/profile',
     loadChildren: () => import('./publisher/profile/profile.module').then(m => m.PublisherProfileModule),
-    pathMatch: 'full'
   },
   {
     path: 'publisher/courses',
     loadChildren: () => import('./publisher/courses/courses.module').then(m => m.PublisherCoursesModule),
-    pathMatch: 'full'
   },
   {
     path: 'publisher/discount-codes',
     loadChildren: () => import('./publisher/discount-codes/discount-codes.module').then(m => m.DiscountCodesModule),
-    pathMatch: 'full'
-  }
+  },
 ]
 
 
