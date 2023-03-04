a87096e8719799fc12ac27ebebf654b242270a22
Sławomir Borkowski <slawomir@bytech.ca>
2023-03-04 13:43:13 +0100

    expose owner_uid in laravel publisher model

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Publisher extends Model
 {
-    protected $hidden = ['owner_uid'];
     protected $casts = ["settings" => "array"];
 
     use HasFactory;
