95641720f47c9f779ae36a9062590a8f269391c8
Sławomir Borkowski <slawomir@bytech.ca>
2023-03-04 14:10:56 +0100

    security checks in codesUseController

--- app/Http/Controllers/CodesUseController.php
+++ app/Http/Controllers/CodesUseController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\CodesUse;
+use App\Models\Course;
+use App\Models\Coupon;
 
 class CodesUseController extends Controller
 {
     public function access_code_users($code_id) {
-        return CodesUse::where('code_id', $code_id)->with('user')->get();
+        $uid = auth()->user()->id;
+        $code = Coupon::find($code_id);
+        $course = Course::where('id', $code->cid)->with('publisher')->first();
+
+        if ($uid === $course['publisher']['owner_uid']) {
+            return CodesUse::where('code_id', $code_id)->with('user')->get();
+        }
+
+        return [];
     }
 }
