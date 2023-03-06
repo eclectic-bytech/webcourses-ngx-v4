19d8347414ab0e3c0d834530d559f4bbe1e033b8
SirL3X0N <seth@bytech.ca>
2023-03-06 14:42:29 +0100

    cleaned up code nicely

--- app/Listeners/LoginListener.php
+++ app/Listeners/LoginListener.php
@@ -7,31 +7,12 @@ use Illuminate\Queue\InteractsWithQueue;
 
 class LoginListener
 {
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-    }
-    public function incrementLoginCount($event) {
-        $UserID = ($event->user->id);
-        $event->user::find($UserID)->increment('login_count');
-    }
-
-    /**
-     * Handle the event.
-     *
-     * @param  object  $event
-     * @return void
-     */
     public function handle($event)
     {
         $event->user->update([
             'last_login_time' => now(),
             'last_login_ip' => request()->GetClientIp(),
-            'login_count' => $this->incrementLoginCount($event)
+            'login_count' => $event->user->increment('login_count')
         ]);
     }
 }
