899e139b63b7724194d3c66526e2765a82bf5350
Sławomir Borkowski <slawomir@bytech.ca>
2023-03-06 12:08:20 +0100

    call to api from angular to user-progress report

--- resources/webcourses/src/app/pages/admin/publisher/course/user-progress/user-progress.component.ts
+++ resources/webcourses/src/app/pages/admin/publisher/course/user-progress/user-progress.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ConfigService } from 'src/app/core/services/config/config.service';
 
 @Component({
   selector: 'app-user-progress',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProgressComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private configService: ConfigService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.httpClient.get<any[]>(
+      `${this.configService.params.api.route}/admin/publisher/course/user-progress/${this.pid}`
+    ).subscribe(
+      (data) => {
+        console.log(data)
+        console.log('aaa')
+      }
+    )
+  }
+
+  get pid() {
+    return this.route.snapshot.paramMap.get('pid')
   }
 
 }
