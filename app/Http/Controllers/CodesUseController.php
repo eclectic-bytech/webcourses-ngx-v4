7cd861c54e5a5206dea386dc7f48419b712f0b10
Sławomir Borkowski <slawomir@bytech.ca>
2023-03-07 13:46:41 +0100

    changed find to where in codesuse controller

--- app/Http/Controllers/CodesUseController.php
+++ app/Http/Controllers/CodesUseController.php
@@ -29,7 +29,7 @@ class CodesUseController extends Controller
     }
 
     public function access_code_course($code_id) {
-        $access_code = Coupon::find($code_id)->first();
+        $access_code = Coupon::where('id', $code_id)->first();
         return Course::where('id', $access_code->cid)->withCount('courseSyllabus as total_activities')->first();
     }
 }
