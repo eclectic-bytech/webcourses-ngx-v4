e2ea374c3c07d103d378f252a881b578a3cb5591
Sławomir Borkowski <slawomir@bytech.ca>
2023-03-11 22:56:44 +0100

    imported notification into reg listener

--- app/Listeners/RegistrationListener.php
+++ app/Listeners/RegistrationListener.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Notification;
 use App\Notifications\RegistrationNotification;
 
 class RegistrationListener
@@ -26,6 +27,6 @@ class RegistrationListener
      */
     public function handle($event)
     {
-        \Notification::send($event->user, new RegistrationNotification());
+        Notification::send($event->user, new RegistrationNotification());
     }
 }
