545ccabab7d32de41f3f4359415c675f3cefe5f6
Sławomir Borkowski <slawomir@bytech.ca>
2023-03-14 00:30:01 +0100

    stat refresh buttons in admin dashboard

--- resources/webcourses/src/app/pages/admin/system/dashboard/dashboard.component.ts
+++ resources/webcourses/src/app/pages/admin/system/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core'
 import { Observable } from 'rxjs'
+import { faRepeat } from '@fortawesome/free-solid-svg-icons'
+
+// WNGX imports
 import { FadeInOut } from 'src/app/core/animations/fade-in-out.animation'
+import { DashboardService } from './dashboard.service'
+
 import { JetstreamUser } from 'src/app/core/models/jetstream-user.model'
 import { Coupon } from 'src/app/models/coupon.model'
-import { DashboardService } from './dashboard.service'
 
 @Component({
   selector: 'app-dashboard',
@@ -14,11 +18,24 @@ import { DashboardService } from './dashboard.service'
 
 export class DashboardComponent {
 
+  public faRepeat = faRepeat
+
   public recentCodeUses$: Observable<Coupon[]> = this.dashboardService.recentCodeUses()
   public recentLogins$: Observable<JetstreamUser[]> = this.dashboardService.recentLogins()
+  public lastRefresh = new Date()
+  public lastLatestLoginsRefresh = new Date()
 
   constructor(
-    private dashboardService: DashboardService
+    public dashboardService: DashboardService
   ) { }
 
+  refresh() {
+    this.recentCodeUses$ = this.dashboardService.recentCodeUses()
+    this.lastRefresh = new Date()
+  }
+
+  refreshLatestLogins() {
+    this.recentLogins$ = this.dashboardService.recentLogins()
+    this.lastLatestLoginsRefresh = new Date()
+  }
 }
--- resources/webcourses/src/app/pages/admin/system/dashboard/dashboard.module.ts
+++ resources/webcourses/src/app/pages/admin/system/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 // WNGX imports
 import { DashboardComponent } from './dashboard.component'
@@ -13,6 +14,7 @@ import { LoadingSpinnerModule } from 'src/app/core/modules/loading-spinner/loadi
   ],
   imports: [
     CommonModule,
+    FontAwesomeModule,
     DashboardRoutingModule,
     LoadingSpinnerModule
   ]
