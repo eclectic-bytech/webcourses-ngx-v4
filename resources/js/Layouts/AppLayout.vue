867529688de2550c8b363b1c745ed5567a3e7722
SirL3X0N <seth@bytech.ca>
2023-03-13 11:45:35 +0100

    Cleaned up display

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -96,22 +96,13 @@
                             <div class="ml-3 relative">
                                 <jet-dropdown align="right" width="48">
                                     <template #trigger>
-                                        <button v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                        <button class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
                                             <div class="inline-flex items-center">
-                                            <div class="mr-2">{{ $page.props.user.username }}</div>
-                                            <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.user.profile_photo_url"/>
+                                                <div class="text-center mr-2">{{ $page.props.user.username }}</div>
+                                                <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.user.profile_photo_url"/>
+                                                <font-awesome-icon icon="fa-solid fa-caret-down" class="ml-1 text-zinc-900" />
                                             </div>
                                         </button>
-
-                                        <span v-else class="inline-flex rounded-md">
-                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
-                                                {{ $page.props.user.username }}
-
-                                                <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                                </svg>
-                                            </button>
-                                        </span>
                                     </template>
 
                                     <template #content>
--- resources/js/app.js
+++ resources/js/app.js
@@ -12,10 +12,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faGraduationCap, faRightFromBracket, faWrench} from '@fortawesome/free-solid-svg-icons'
+import {faGraduationCap, faRightFromBracket, faWrench,  faCaretDown} from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faGraduationCap , faRightFromBracket, faWrench)
+library.add(faGraduationCap , faRightFromBracket, faWrench, faCaretDown)
 
 const el = document.getElementById('app');
 
