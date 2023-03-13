ece401e4f6d423f8e2357390aa98fba9e78002c8
SirL3X0N <seth@bytech.ca>
2023-03-13 15:36:50 +0100

    added left hand small screen dropdown

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -7,9 +7,18 @@
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
+                           <!-- Hamburger -->
+                           <div class="-mr-2 flex items-center sm:hidden">
+                            <button @click="showingNavigationDropdown = ! showingNavigationDropdown" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
+                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                    <path :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                                    <path :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
                         <div class="flex">
                             <!-- Logo -->
-                            <div class="flex-shrink-0 flex items-center">
+                            <div class="flex-shrink-0 flex items-center sm:-my-px sm:flex">
                                 <inertia-link :href="route('dashboard')">
                                     <!--
                                         Preserving this link to help understand Vue, when we move to
@@ -177,52 +186,11 @@
                 <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
 
                     <!-- Responsive Settings Options -->
-                    <div class="pt-4 pb-1 border-t border-gray-200">
-                        <div class="flex items-center px-4">
-                            <div v-if="$page.props.jetstream.managesProfilePhotos" class="flex-shrink-0 mr-3" >
-                                <img class="h-10 w-10 rounded-full object-cover" :src="$page.props.user.profile_photo_url"/>
-                            </div>
-
-                            <div>
-                                <div class="font-medium text-base text-gray-800">{{ $page.props.user.username }}</div>
-                                <div class="font-medium text-sm text-gray-500">{{ $page.props.user.email }}</div>
-                            </div>
-                        </div>
-
+                    <div class="pt-1 pb-2 border-b border-gray-200">
                         <div class="mt-3 space-y-1">
-                            <jet-responsive-nav-link :href="('/user/redirect')">
-                                <div class="inline-flex py-1">
-                                    <div class="fa-icon-container">
-                                         <font-awesome-icon icon="fa-solid fa-graduation-cap" class="text-amber-400 text-xlg" />
-                                     </div>
-                                    My Courses
-                                   </div>
-                            </jet-responsive-nav-link>
-
-                            <jet-responsive-nav-link :href="route('profile.show')" :active="route().current('profile.show')">
-                                <div class="inline-flex py-1">
-                                    <div class="fa-icon-container">
-                                         <font-awesome-icon icon="fa-solid fa-wrench" class="text-amber-400 text-xlg" />
-                                     </div>
-                                    Profile
-                                   </div>
-                            </jet-responsive-nav-link>
-
-                            <jet-responsive-nav-link :href="route('api-tokens.index')" :active="route().current('api-tokens.index')" v-if="$page.props.jetstream.hasApiFeatures">
-                                API Tokens
-                            </jet-responsive-nav-link>
-
-                            <!-- Authentication -->
-                            <form method="POST" @submit.prevent="logout">
-                                <jet-responsive-nav-link as="button">
-                                    <div class="inline-flex py-1">
-                                        <div class="fa-icon-container">
-                                        <font-awesome-icon icon="fa-solid fa-right-from-bracket" class="text-amber-400 text-xlg" />
-                                    </div>
-                                    Sign Out
-                                   </div>
-                                </jet-responsive-nav-link>
-                            </form>
+                            <a href="/webcourses/catalogue" class="py-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition">Webcourses</a>
+                            <a href="https://publish.cultivatelearning.ca/" class="py-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition">Publish</a>
+                            <a href="/webcourses/help" class="py-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition">Help</a>
 
                             <!-- Team management in hamburger menu
                             <!~~ Team Management ~~>
