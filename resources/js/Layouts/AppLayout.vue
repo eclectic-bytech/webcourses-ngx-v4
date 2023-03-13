75a55b363990f7f8b1648f75b5e62b6cb3a49905
SirL3X0N <seth@bytech.ca>
2023-03-13 11:55:20 +0100

    Added gravatar to small dropdown replacing burger

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -161,10 +161,14 @@
                         <!-- Hamburger -->
                         <div class="-mr-2 flex items-center sm:hidden">
                             <button @click="showingNavigationDropdown = ! showingNavigationDropdown" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <div class="inline-flex items-center">
+                                    <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.user.profile_photo_url"/>
+                                    <font-awesome-icon icon="fa-solid fa-caret-down" class="ml-1 text-zinc-900" />
+                                </div>
+                                <!-- <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                     <path :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                                     <path :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
+                                </svg> -->
                             </button>
                         </div>
                     </div>
