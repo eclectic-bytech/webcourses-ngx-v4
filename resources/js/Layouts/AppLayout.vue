bc0593ec509baf281e5780fc6e91a0ee4651b4e8
SirL3X0N <seth@bytech.ca>
2023-03-13 12:08:18 +0100

    clean up on previous commit

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -96,11 +96,11 @@
                             <div class="ml-3 relative">
                                 <jet-dropdown align="right" width="48">
                                     <template #trigger>
-                                        <button class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
                                             <div class="inline-flex items-center">
                                                 <div class="text-center mr-2">{{ $page.props.user.username }}</div>
-                                                <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.user.profile_photo_url"/>
-                                                <font-awesome-icon icon="fa-solid fa-caret-down" class="ml-1 text-zinc-900" />
+                                                <img class="h-10 w-auto rounded-full object-cover" :src="$page.props.user.profile_photo_url"/>
+                                                <font-awesome-icon icon="fa-solid fa-caret-down" class="ml-1 text-gray-700" />
                                             </div>
                                         </button>
                                     </template>
@@ -162,8 +162,8 @@
                         <div class="-mr-2 flex items-center sm:hidden">
                             <button @click="showingNavigationDropdown = ! showingNavigationDropdown" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
                                 <div class="inline-flex items-center">
-                                    <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.user.profile_photo_url"/>
-                                    <font-awesome-icon icon="fa-solid fa-caret-down" class="ml-1 text-zinc-900" />
+                                    <img class="h-10 w-auto rounded-full object-cover" :src="$page.props.user.profile_photo_url"/>
+                                    <font-awesome-icon icon="fa-solid fa-caret-down" class="ml-1 text-gray-700" />
                                 </div>
                                 <!-- <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                     <path :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -186,7 +186,7 @@
                     <div class="pt-4 pb-1 border-t border-gray-200">
                         <div class="flex items-center px-4">
                             <div v-if="$page.props.jetstream.managesProfilePhotos" class="flex-shrink-0 mr-3" >
-                                <img class="h-10 w-10 rounded-full object-cover" :src="$page.props.user.profile_photo_url" :alt="$page.props.user.username" />
+                                <img class="h-10 w-10 rounded-full object-cover" :src="$page.props.user.profile_photo_url"/>
                             </div>
 
                             <div>
