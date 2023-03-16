07e38895d5a3831802cfc07b2c0dd3726cc3f366
Sławomir Borkowski <slawomir@bytech.ca>
2023-03-16 14:36:03 +0100

    download cert ui tweaks

--- resources/webcourses/src/app/pages/webcourse/activities/workarea/activities/special-activity/completion-cert/completion-cert.component.ts
+++ resources/webcourses/src/app/pages/webcourse/activities/workarea/activities/special-activity/completion-cert/completion-cert.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core'
 import { CompletionCertService } from './completion-cert.service'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faSpinner, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-completion-cert',
   templateUrl: './completion-cert.component.html',
-  styleUrls: ['./completion-cert.component.sass']
+  styleUrls: ['./completion-cert.component.scss']
 })
 export class CompletionCertComponent implements OnInit {
 
   faSpinner = faSpinner
+  faFileArrowDown = faFileArrowDown
 
   constructor(
     public completionCertService: CompletionCertService
