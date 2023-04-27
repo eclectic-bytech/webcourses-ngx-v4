23905a981f39e75e7bb0470b433a0ad23471c185
Sławomir Borkowski <slawomir@bytech.ca>
2023-04-27 09:36:17 +0200

    catalogue-card converted from bootstrap card to matcard

--- resources/webcourses/src/app/pages/components/course-card/catalogue-card/catalogue-card.module.ts
+++ resources/webcourses/src/app/pages/components/course-card/catalogue-card/catalogue-card.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { CatalogueCardComponent } from './catalogue-card.component'
 import { RouterModule } from '@angular/router'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+
+import { CatalogueCardComponent } from './catalogue-card.component'
 import { BuyButtonModule } from '../../buttons/buy-button/buy-button.module'
 import { PipesModule } from './../../../../shared/pipes/pipes.module'
 import { CourseAccessBlockComponent } from './course-access-block/course-access-block.component'
@@ -11,6 +14,8 @@ import { CourseAccessBlockComponent } from './course-access-block/course-access-
   imports: [
     CommonModule,
     RouterModule,
+    MatCardModule,
+    MatButtonModule,
     BuyButtonModule,
     PipesModule
   ],
