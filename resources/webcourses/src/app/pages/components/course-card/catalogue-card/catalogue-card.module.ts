d15634fc59c2239004cd7d922babeaf2db603dea
Sławomir Borkowski <slawomir@bytech.ca>
2023-04-27 13:49:37 +0200

    catalogue card updates

--- resources/webcourses/src/app/pages/components/course-card/catalogue-card/catalogue-card.module.ts
+++ resources/webcourses/src/app/pages/components/course-card/catalogue-card/catalogue-card.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 import { CatalogueCardComponent } from './catalogue-card.component'
 import { BuyButtonModule } from '../../buttons/buy-button/buy-button.module'
 import { PipesModule } from './../../../../shared/pipes/pipes.module'
 import { CourseAccessBlockComponent } from './course-access-block/course-access-block.component'
+
 
 @NgModule({
   declarations: [CatalogueCardComponent, CourseAccessBlockComponent],
@@ -16,6 +18,7 @@ import { CourseAccessBlockComponent } from './course-access-block/course-access-
     RouterModule,
     MatCardModule,
     MatButtonModule,
+    FontAwesomeModule,
     BuyButtonModule,
     PipesModule
   ],
