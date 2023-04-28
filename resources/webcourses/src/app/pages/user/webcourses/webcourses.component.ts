7e077ff64fa7c44c0978d3c65406711fa901023e
Sławomir Borkowski <slawomir@bytech.ca>
2023-04-28 14:52:22 +0200

    my webcourses section overhaul start

--- resources/webcourses/src/app/pages/user/webcourses/webcourses.component.ts
+++ resources/webcourses/src/app/pages/user/webcourses/webcourses.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
+import { Observable } from 'rxjs'
 import { AppService } from '../../../app.service'
 import { UserService } from '../../../core/services/user/user.service'
 import { FadeInOut } from '../../../core/animations/fade-in-out.animation'
 import { AccessCodeModalService } from '../../components/access-code-modal/access-code-modal.service'
+import { JetstreamUser } from 'src/app/core/models/jetstream-user.model'
+import { GravatarMd5Service } from 'src/app/core/services/gravatar-md5/gravatar-md5.service'
 
 @Component({
   selector: 'app-webcourses',
@@ -14,9 +17,12 @@ import { AccessCodeModalService } from '../../components/access-code-modal/acces
 export class WebcoursesComponent implements OnInit {
 
   public showGroupCodeInput = false
+  // public user$ = new Observable<JetstreamUser>
+  public user$: Observable<JetstreamUser> = this.user.getUser()
 
   constructor(
     private route: ActivatedRoute,
+    public gravatarMd5: GravatarMd5Service,
     private appService: AppService,
     private accessCodeModalService: AccessCodeModalService,
     public user: UserService
--- resources/webcourses/src/app/pages/user/webcourses/webcourses.module.ts
+++ resources/webcourses/src/app/pages/user/webcourses/webcourses.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { GravatarModule } from 'ngx-gravatar'
+
 import { WebcoursesComponent } from './webcourses.component'
 import { WebcoursesRoutingModule } from './webcourses-routing.module'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { CatalogueCardModule } from '../../components/course-card/catalogue-card/catalogue-card.module'
 import { GroupJoinCardModule } from '../../components/group-join-card/group-join-card.module'
 import { LoadingSpinnerModule } from '../../../core/modules/loading-spinner/loading-spinner.module'
@@ -14,9 +16,9 @@ import { LoadingSpinnerModule } from '../../../core/modules/loading-spinner/load
   imports: [
     CommonModule,
     NgbModule,
+    GravatarModule,
     WebcoursesRoutingModule,
     CatalogueCardModule,
-    GroupJoinCardModule,
     LoadingSpinnerModule
   ]
 })
