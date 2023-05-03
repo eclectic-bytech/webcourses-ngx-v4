338fe078989433ee3153110eb582990ed3346f6a
SirL3X0N <seth@bytech.ca>
2023-05-03 11:01:17 +0200

    Demo done

--- resources/webcourses/src/app/pages/demo/demo.component.ts
+++ resources/webcourses/src/app/pages/demo/demo.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 
 import { FadeInOut } from 'src/app/core/animations/fade-in-out.animation'
 import { ConfigService } from 'src/app/core/services/config/config.service'
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -11,10 +12,17 @@ import { ConfigService } from 'src/app/core/services/config/config.service'
   styleUrls: ['./demo.component.scss']
 })
 export class DemoComponent {
-
+  htmlContent: string
   constructor(
-    public configService: ConfigService
+    public configService: ConfigService,
+    private http: HttpClient
   ) { }
+
+  ngOnInit() {
+    this.http.get('/webcourses/publisher-files/default/html/demo.html', { responseType: 'text' }).subscribe(data => {
+      this.htmlContent = data
+    })
+  }
 
   demoButton() {
     window.location.href = `/user/register?code=${this.configService.params.freeAccessCode}`
