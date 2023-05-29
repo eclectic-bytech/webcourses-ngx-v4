2ce7b471b2b70ed7f97ad11721034049352780ac
SirL3X0N <seth@bytech.ca>
2023-05-30 00:18:55 +0200

    She's one giant mess but thats how we like them

---
+++ database/migrations/2023_05_27_221520_updating_selected_aid_on_user_progress_table_with_selected_aid.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\UserProgress;
+use App\Models\UserAnswer;
+use App\Models\CourseSyllabus;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $userProgresses = UserProgress::all();
+        $userAnswers = UserAnswer::all();
+        $courseSyllabuses = CourseSyllabus::all();
+
+        foreach ($userProgresses as $userProgress) {
+            $pid = $userProgress->id;
+            $proid = null;
+
+            foreach ($userAnswers as $userAnswer) {
+                if ($userAnswer->progress_id === $pid) {
+                    $proid = $userAnswer->activity_id;
+                }
+            }
+            if ($proid === null) {
+                $upccid = $userProgress->course_id;
+                foreach ($courseSyllabuses as $courseSyllabus) {
+                    if ($courseSyllabus->course_id === $upccid && $courseSyllabus->seq === 0) {
+                        $proid = $courseSyllabus->activity_id;
+                    }
+                }
+            }
+            $userProgress->selected_aid  = $proid;
+            $userProgress->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
+
