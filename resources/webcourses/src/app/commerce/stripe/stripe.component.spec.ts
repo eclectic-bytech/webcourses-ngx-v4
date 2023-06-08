c97d6bbcfe59ae7360f349f20959384510d9ea60
Sławomir Borkowski <slawomir@bytech.ca>
2023-06-08 15:25:38 +0200

    stripe boilerplate

---
+++ resources/webcourses/src/app/commerce/stripe/stripe.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+
+import { StripeComponent } from './stripe.component'
+
+describe('StripeComponent', () => {
+  let component: StripeComponent
+  let fixture: ComponentFixture<StripeComponent>
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StripeComponent ]
+    })
+    .compileComponents()
+
+    fixture = TestBed.createComponent(StripeComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+})
---
+++ resources/webcourses/src/app/commerce/stripe/stripe.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { StripeComponent } from './stripe.component'
+
+@NgModule({
+  declarations: [
+    StripeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StripeModule { }
