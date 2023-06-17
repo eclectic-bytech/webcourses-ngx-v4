771ae092ef7a6ca568cbc651f461b550897d0059
Sławomir Borkowski <slawomir@bytech.ca>
2023-06-17 17:39:17 +0200

    first attempt at coupon insert from webhook

--- app/Listeners/StripeEventListener.php
+++ app/Listeners/StripeEventListener.php
@@ -4,6 +4,8 @@ namespace App\Listeners;
 
 use Laravel\Cashier\Events\WebhookReceived;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Coupon;
 
 class StripeEventListener
 {
@@ -16,7 +18,19 @@ class StripeEventListener
     public function handle(WebhookReceived $event)
     {
         if ($event->payload['type'] === 'charge.succeeded') {
-            Log::channel('daily')->debug('Evalis waz here, 2');
+            Log::channel('daily')->debug('First save of a coupon =)');
+            $coupon = new Coupon();
+            $coupon->id = $event->payload['request']['id'];
+            $coupon->code = "TESTER-CODE";
+            $coupon->active = 1;
+            $coupon->label = "TESTER CODE LABEL";
+            $coupon->discount = 0;
+            $coupon->cid = 407;
+            $coupon->gid = 0;
+            $coupon->expiry = "2038-01-19 00:00:00";
+            $coupon->uses = 0;
+            $coupon->uses_max = 3;
+            $coupon->save();
         }
     }
 }
