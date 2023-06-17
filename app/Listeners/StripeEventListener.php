6047e8f41a9789b484cc9702aa65e2271dca6347
Sławomir Borkowski <slawomir@bytech.ca>
2023-06-17 12:32:22 +0200

    stripe event listener added

---
+++ app/Listeners/StripeEventListener.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Listeners;
+
+use Laravel\Cashier\Events\WebhookReceived;
+use IlluminateSupportFacadesLog;
+
+class StripeEventListener
+{
+    /**
+     * Handle received Stripe webhooks.
+     *
+     * @param  \Laravel\Cashier\Events\WebhookReceived  $event
+     * @return void
+     */
+    public function handle(WebhookReceived $event)
+    {
+        if ($event->payload['type'] === 'charge.succeeded') {
+            Log::info('Evalis waz here.');
+        }
+    }
+}
