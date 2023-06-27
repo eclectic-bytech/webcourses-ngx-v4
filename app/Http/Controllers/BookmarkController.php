41abecfacc229610476c1a8066c8fa3aacdf9a7d
SirL3X0N <seth@bytech.ca>
2023-06-27 21:56:51 +0200

    got the basic deta entry working

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\CourseSyllabus;
+use App\Models\ActivityBookmark;
+use App\Models\UserProgress;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class BookmarkController extends Controller
 {
@@ -33,5 +36,13 @@ class BookmarkController extends Controller
             $output = implode(',', $output);
 
         echo "$output";
+
+        $user = auth()->user();
+        $pid = UserProgress::where('user_id',$user->id)->where('selected_aid',$aid)->first();
+
+        $bookmark = new ActivityBookmark();
+        $bookmark->aid = $aid;
+        $bookmark->pid = $pid->id;
+        $bookmark->save();
     }
 }
