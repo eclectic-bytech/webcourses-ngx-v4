0ff5d2ebbf7d39ddd4ad226d42c32dd2b6753ca2
SirL3X0N <seth@bytech.ca>
2023-06-28 00:07:21 +0200

    Delete functionality

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -31,18 +31,18 @@ class BookmarkController extends Controller
      */
     public function show($aid)
     {
-        $output = $aid;
-        if (is_array($output))
-            $output = implode(',', $output);
-
-        echo "$output";
-
         $user = auth()->user();
-        $pid = UserProgress::where('user_id',$user->id)->where('selected_aid',$aid)->first();
+        $pid = UserProgress::where('user_id',$user->id)->where('selected_aid',$aid)->first()->id;
 
-        $bookmark = new ActivityBookmark();
-        $bookmark->aid = $aid;
-        $bookmark->pid = $pid->id;
-        $bookmark->save();
+        $isBookmarked = ActivityBookmark::where('pid',$pid)->where('aid',$aid)->first() || null;
+
+        if ($isBookmarked == null){
+            $bookmark = new ActivityBookmark();
+            $bookmark->aid = $aid;
+            $bookmark->pid = $pid;
+            $bookmark->save();
+        }else{
+            ActivityBookmark::where('pid',$pid)->where('aid',$aid)->first()->delete();
+        }
     }
 }
