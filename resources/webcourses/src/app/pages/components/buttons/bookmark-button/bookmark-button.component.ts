ec52815e8a08d0ecc840838344522482a4bcc9f8
Sławomir Borkowski <slawomir@bytech.ca>
2023-06-27 11:06:13 +0200

    passing activityMeta to bookmark component

--- resources/webcourses/src/app/pages/components/buttons/bookmark-button/bookmark-button.component.ts
+++ resources/webcourses/src/app/pages/components/buttons/bookmark-button/bookmark-button.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { ConfigService } from 'src/app/core/services/config/config.service';
+import { ActivityMeta } from 'src/app/pages/webcourse/activities/workarea/models/activity-meta.model';
 
 @Component({
   selector: 'app-bookmark-button',
@@ -10,12 +11,15 @@ import { ConfigService } from 'src/app/core/services/config/config.service';
 
 export class BookmarkButtonComponent {
 
+  @Input() activityMeta: ActivityMeta
+
   constructor(
     private httpClient: HttpClient,
     private configService: ConfigService,
   ) { }
 
   bookmarkActivity() {
+    console.log(this.activityMeta)
     const aid = 4764
     this.httpClient.put<number>(
       `${this.configService.params.api.route}/admin/publisher/course-editor/syllabus/${aid}/bookmark`, aid
