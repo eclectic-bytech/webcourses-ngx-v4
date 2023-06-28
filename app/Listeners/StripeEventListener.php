b2dcb1a9a6857d032d3e0d6bc101f3b1965eab3c
Sławomir Borkowski <slawomir@bytech.ca>
2023-06-28 12:51:01 +0200

    loading sale in stripe webhook

--- app/Listeners/StripeEventListener.php
+++ app/Listeners/StripeEventListener.php
@@ -18,24 +18,29 @@ class StripeEventListener
      */
     public function handle(WebhookReceived $event)
     {
+
         if ($event->payload['type'] === 'charge.succeeded') {
-            $sale = Sale::where('payment_intent', $event->payload['payment_intent'])->first();
+
+            $sale = Sale::where('payment_intent', $event->payload['data']['object']['payment_intent'])->first();
             $sale->charge_object = $event->payload;
             $sale->save();
 
-            Log::channel('daily')->debug(`Get UID:`.$sale->uid.` access to CID:` . $sale->cid . '.');
-            $coupon = new Coupon();
-            $coupon->id = md5($event->payload['request']['id']);
-            $coupon->code = $event->payload['request']['id'];
-            $coupon->active = 1;
-            $coupon->label = "MD5 TESTER CODE LABEL";
-            $coupon->discount = 0;
-            $coupon->cid = 407;
-            $coupon->gid = 1;
-            $coupon->expiry = "2038-01-19 00:00:00";
-            $coupon->uses = 0;
-            $coupon->uses_max = 3;
-            $coupon->save();
+            Log::channel('daily')->info("Get UID:" . $sale->uid . " access to CID:" . $sale->cid . ".");
+            // $coupon = new Coupon();
+            // $coupon->id = md5($event->payload['request']['id']);
+            // $coupon->code = $event->payload['request']['id'];
+            // $coupon->active = 1;
+            // $coupon->label = "MD5 TESTER CODE LABEL";
+            // $coupon->discount = 0;
+            // $coupon->cid = 407;
+            // $coupon->gid = 1;
+            // $coupon->expiry = "2038-01-19 00:00:00";
+            // $coupon->uses = 0;
+            // $coupon->uses_max = 3;
+            // $coupon->save();
+        } else {
+            Log::channel('daily')->info("Payment failed, or something");
         }
+
     }
 }
