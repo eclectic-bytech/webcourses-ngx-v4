b89b2f92e135f1da95a5671aebd303a5e18350af
Sławomir Borkowski <slawomir@bytech.ca>
2023-06-30 10:16:14 +0200

    resume button and login redirect on buy button

--- resources/webcourses/src/app/commerce/stripe/stripe-checkout/stripe-checkout.component.ts
+++ resources/webcourses/src/app/commerce/stripe/stripe-checkout/stripe-checkout.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Component, Input } from '@angular/core'
 import { ConfigService } from 'src/app/core/services/config/config.service'
+import { UserService } from 'src/app/core/services/user/user.service'
 
 @Component({
   selector: 'app-stripe-checkout',
@@ -14,16 +15,21 @@ export class StripeCheckoutComponent {
 
   constructor(
     private httpClient: HttpClient,
-    private configService: ConfigService
+    private configService: ConfigService,
+    private userService: UserService
   ) {}
 
   public BuyButton() {
-    this.httpClient.get(
-      `${this.configService.params.api.route}/commerce/stripe/checkout/course/${this.cid}`
-    ).subscribe(
-      (checkoutUrl: string) => {
-        window.location.href = checkoutUrl
-      }
-    )
+    if (!this.userService.user) {
+      window.location.href = '/user/login'
+    } else {
+      this.httpClient.get(
+        `${this.configService.params.api.route}/commerce/stripe/checkout/course/${this.cid}`
+      ).subscribe(
+        (checkoutUrl: string) => {
+          window.location.href = checkoutUrl
+        }
+      )
+    }
   }
 }
