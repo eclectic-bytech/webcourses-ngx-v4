13e307eaee78cc7bb2e690d5d365303b4d61b324
SirL3X0N <seth@bytech.ca>
2023-07-20 19:56:45 +0200

    call to the backend, cid is hard coded in the component

--- resources/webcourses/src/app/pages/webcourse/activities/sidebar/bookmarks/bookmarks.component.ts
+++ resources/webcourses/src/app/pages/webcourse/activities/sidebar/bookmarks/bookmarks.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookmarksService } from './bookmarks.service';
+import { Bookmark } from './bookmark.model';
 
 @Component({
   selector: 'app-bookmarks',
   templateUrl: './bookmarks.component.html',
   styleUrls: ['./bookmarks.component.sass']
 })
-export class BookmarksComponent {
-
+export class BookmarksComponent implements OnInit {
+  bookmarkIndex: Bookmark[]
+  constructor(
+    public bookmarksService: BookmarksService,
+  ) { }
+  ngOnInit() {
+    this.bookmarksService.getChapterIndex(1)
+  }
 }
--- resources/webcourses/src/app/pages/webcourse/activities/sidebar/bookmarks/bookmarks.service.ts
+++ resources/webcourses/src/app/pages/webcourse/activities/sidebar/bookmarks/bookmarks.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { ConfigService } from 'src/app/core/services/config/config.service'
+import { Bookmark } from './bookmark.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookmarksService {
 
-  constructor() { }
+  public selectedBookmark: Bookmark[]
+
+  constructor(
+    private httpClient: HttpClient,
+    private configService: ConfigService
+  ) { }
+
+  getChapterIndex(cid: number) {
+    console.log(cid)
+    return this.httpClient.get<Bookmark[]>(
+      `${this.configService.params.api.route}/webcourse/${cid}/bookmarks`
+    ).subscribe((response) => { this.selectedBookmark = response })
+  }
+
+  get bookmarkIndex() {
+    return this.selectedBookmark
+  }
 }
