c72335eb4bcf0a86297d59bb48239c8ab6560aa4
SirL3X0N <seth@bytech.ca>
2023-07-22 12:47:02 +0200

    Made bookmark cards display label and aid

--- resources/webcourses/src/app/pages/webcourse/activities/sidebar/shared/bookmark-card/bookmark-card.component.ts
+++ resources/webcourses/src/app/pages/webcourse/activities/sidebar/shared/bookmark-card/bookmark-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Bookmark } from '../../bookmarks/bookmark.model';
 
 @Component({
   selector: 'app-bookmark-card',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./bookmark-card.component.sass']
 })
 export class BookmarkCardComponent {
-
+  @Input() bookmark: Bookmark
 }
