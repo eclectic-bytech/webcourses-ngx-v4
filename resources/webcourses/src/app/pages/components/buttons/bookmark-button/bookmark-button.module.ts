eab9f092d7074ef32fcc6164c90111501bf1bb46
SirL3X0N <seth@bytech.ca>
2023-07-22 15:37:48 +0200

    remove useless import

--- resources/webcourses/src/app/pages/components/buttons/bookmark-button/bookmark-button.module.ts
+++ resources/webcourses/src/app/pages/components/buttons/bookmark-button/bookmark-button.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookmarkButtonComponent } from './bookmark-button.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     BookmarkButtonComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule
+    CommonModule
   ],
   exports: [
     BookmarkButtonComponent
