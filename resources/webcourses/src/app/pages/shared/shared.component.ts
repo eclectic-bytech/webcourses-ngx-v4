ec472c6af316f3b61f471e6b6d0f1ee41c176a2e
Sławomir Borkowski <slawomir@bytech.ca>
2023-09-07 18:11:11 +0200

    tweaks and reactor of pages shared component

--- resources/webcourses/src/app/pages/shared/shared.component.ts
+++ resources/webcourses/src/app/pages/shared/shared.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { FadeInOut } from 'src/app/core/animations/fade-in-out.animation'
 import { HttpClient } from '@angular/common/http'
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-shared',
@@ -11,28 +11,31 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./shared.component.sass']
 })
 export class SharedComponent implements OnInit, OnDestroy {
-  htmlContent: string
-  private paramMapSubscription: Subscription;
+  private sub: Subscription
+  public htmlContent: string
 
   constructor(
     private httpClient: HttpClient,
-    private _activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    this.paramMapSubscription = this._activatedRoute.paramMap.subscribe(params => {
-      let selectedPage = params.get('page') || 'about'
-      if (selectedPage == 'getstarted') {
-        selectedPage = 'publish'
-      }
-      this.httpClient.get(
-        '/webcourses/publisher-files/default/html/pages/' + selectedPage + '.html', { responseType: 'text' }
-      ).subscribe(htmlContent => {
-        this.htmlContent = htmlContent
-      })
-    });
+    this.sub = this.activatedRoute.paramMap.subscribe( params => {
+      // home page is at app root ("no path") and is titled 'about'
+      let selectedPage = (params.get('page')) ? (params.get('page')) : 'about'
+      this.loadPage( selectedPage === 'getstarted' ? 'publish' : selectedPage )
+    })
   }
+
+  loadPage(selectedPage: string) {
+    this.httpClient.get(
+      `/webcourses/publisher-files/1/html/pages/${selectedPage}.html`, { responseType: 'text' }
+    ).subscribe( htmlContent => {
+      this.htmlContent = htmlContent
+    })
+  }
+
   ngOnDestroy(): void {
-    this.paramMapSubscription.unsubscribe();
+    this.sub.unsubscribe()
   }
 }
