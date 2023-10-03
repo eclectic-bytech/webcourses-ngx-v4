8d8b08c273e329e3934b9385f8d3f780f1ed0045
SirL3X0N <seth@bytech.ca>
2023-10-03 14:14:41 +0200

    Added if active fix

--- resources/webcourses/src/app/pages/catalogue/course-filter-button/course-filter-button.component.ts
+++ resources/webcourses/src/app/pages/catalogue/course-filter-button/course-filter-button.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 import { faFilter } from '@fortawesome/free-solid-svg-icons'
-import { CatalogueService } from '../catalogue.service';
+import { CatalogueService } from '../catalogue.service'
 
 @Component({
   selector: 'app-course-filter-button',
@@ -11,6 +11,7 @@ export class CourseFilterButtonComponent {
   public menuCollapsed = true
   public menuCollapsed2 = true
   public faFilter = faFilter
+  private sortID = 0
 
   constructor(
     public catalogueService: CatalogueService
@@ -19,26 +20,43 @@ export class CourseFilterButtonComponent {
     this.menuCollapsed = this.menuCollapsed2 = true
   }
 
-  Alphabetical() {
-    this.catalogueService.webcoursesAll$.subscribe((cat) => {
-      cat.sort((a, b) => a.title.localeCompare(b.title))
-    })
+  sortbyAlphabetical() {
+    if (this.sortID !== 1) {
+      this.catalogueService.webcoursesAll$.subscribe((cat) => {
+        cat.sort((a, b) => a.title.localeCompare(b.title))
+      })
+      this.sortID = 1
+    } else {
+      this.sortbyID()
+    }
   }
 
-  Recent() {
-    this.catalogueService.webcoursesAll$.subscribe((cat) => {
-      cat.sort((a, b) => this.compareDates(a.updated_at, b.updated_at));
-    });
+  sortbyRecent() {
+    if (this.sortID !== 2) {
+      this.catalogueService.webcoursesAll$.subscribe((cat) => {
+        cat.sort((a, b) => this.compareDates(a.updated_at, b.updated_at))
+      })
+      this.sortID = 2
+    } else {
+      this.sortbyID()
+    }
   }
 
   private compareDates(dateA: string | undefined, dateB: string | undefined): number {
-    if (!dateA && !dateB) return 0; // Both are empty, consider them equal
-    if (!dateA) return 1; // a is empty, push it to the end
-    if (!dateB) return -1; // b is empty, push it to the end
+    if (!dateA && !dateB) return 0 // Both are empty, consider them equal
+    if (!dateA) return 1 // a is empty, push it to the end
+    if (!dateB) return -1 // b is empty, push it to the end
 
-    const timestampA = new Date(dateA).getTime();
-    const timestampB = new Date(dateB).getTime();
+    const timestampA = new Date(dateA).getTime()
+    const timestampB = new Date(dateB).getTime()
 
-    return timestampB - timestampA; // Sort in descending order
+    return timestampB - timestampA // Sort in descending order
+  }
+
+  sortbyID() {
+    this.catalogueService.webcoursesAll$.subscribe((cat) => {
+      cat.sort((a, b) => a.id - b.id)
+    })
+    this.sortID = 0
   }
 }
