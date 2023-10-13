c2be12855d3f97c172b516fd1da7514556a6ac28
Sławomir Borkowski <slawomir@bytech.ca>
2023-10-13 13:46:28 +0200

    working request upgrade api call

--- resources/webcourses/src/app/views/default/modals/pub-power-up/pub-power-up.service.ts
+++ resources/webcourses/src/app/views/default/modals/pub-power-up/pub-power-up.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core'
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
 import { PubPowerUpComponent } from './pub-power-up.component'
+import { HttpClient } from '@angular/common/http'
+import { ConfigService } from 'src/app/core/services/config/config.service'
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,9 @@ import { PubPowerUpComponent } from './pub-power-up.component'
 export class PubPowerUpService {
 
   constructor(
-    private ngbModal: NgbModal
+    private httpClient: HttpClient,
+    private ngbModal: NgbModal,
+    private configService: ConfigService
   ) { }
 
   pubPowerUp() {
@@ -18,6 +22,11 @@ export class PubPowerUpService {
   }
 
   requestUpgradeBtn() {
-    console.log('Request upgrade')
+    this.httpClient.post(
+      `${this.configService.params.api.route}/publisher/request-access`, ''
+    ).subscribe(
+      () => { console.log('Access granted!') },
+      (err) => { console.error(err) }
+    )
   }
 }
