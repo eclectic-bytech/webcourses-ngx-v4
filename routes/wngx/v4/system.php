f8bc9c5729be142f6d4bb8d5571070d425853918
Sławomir Borkowski <slawomir@bytech.ca>
2023-10-30 19:46:10 +0100

    implemented UserSAController

--- routes/wngx/v4/system.php
+++ routes/wngx/v4/system.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 use App\Http\Controllers\CouponController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\SystemAdmin\UserSAController;
 use App\Http\Controllers\UserRoleController;
 
 // Paths gruped as /v4/admin...
 Route::middleware(['is_admin'])->prefix('system')->group(function() {
     Route::get('recent-code-uses', [CouponController::class, 'recent_code_uses']);
-    Route::get('recent-logins', [UserController::class, 'recent_logins']);
+    Route::get('recent-logins', [UserSAController::class, 'recent_logins']);
     Route::get('publisher-interest', [UserRoleController::class, 'publisher_interest']);
 });
