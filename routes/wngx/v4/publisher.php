187e87c5d82c2480141c848c6adbb2bb112a0a76
Sławomir Borkowski <slawomir@bytech.ca>
2023-11-02 15:57:16 +0100

    nested access-code routes, codeUsePA introduced

--- routes/wngx/v4/publisher.php
+++ routes/wngx/v4/publisher.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 use App\Http\Controllers\PublisherAdmin\CouponPAController;
-use App\Http\Controllers\CodesUseController;
+use App\Http\Controllers\PublisherAdmin\CodesUsePAController;
 use App\Http\Controllers\UserAnswerController;
 use App\Http\Controllers\CourseEditorController;
 use App\Http\Controllers\PublisherAdmin\CoursePAController;
@@ -13,8 +13,8 @@ use App\Http\Controllers\PublisherAdmin\CoursePAController;
 // Paths grouped as /v4/admin/publisher
 Route::middleware('is_publisher')->prefix('publisher')->group(function() {
 
-    Route::resource('/access-codes', CouponPAController::class)->only(['index']);
-    Route::get('/access-codes/{code_id}/users', [CodesUseController::class, 'access_code_users']);
+    Route::resource('access-codes', CouponPAController::class)->only(['index']);
+    Route::resource('access-codes.users', CodesUsePAController::class)->only(['index']);
 
     Route::resource('/courses', CoursePAController::class)->except(['create', 'edit']);
     Route::put('/course-editor/syllabus/{aid}/demo', [CourseEditorController::class, 'demo']);
