26fc571c1f8642d5ab6569dfdd92378b371f6582
Sławomir Borkowski <slawomir@bytech.ca>
2023-11-22 14:06:02 +0100

    getNextSeq for dynamic seq calc for courseSyllabus

--- app/Services/PublisherServices/CourseSyllabusPublisherService.php
+++ app/Services/PublisherServices/CourseSyllabusPublisherService.php
@@ -6,8 +6,10 @@ use App\Models\CourseSyllabus;
 
 class CourseSyllabusPublisherService {
 
-    public function storeCourseSyllabus($cid, $chid, $aid)
+    public function storeCourseSyllabus($cid, $chid, $aid, $seq = 0)
     {
+        $seq = $seq ? $seq : $this->getNextSeq($cid);
+
         $courseSyllabus = new CourseSyllabus();
 
         $courseSyllabus->activity_id = $aid;
@@ -15,10 +17,15 @@ class CourseSyllabusPublisherService {
         $courseSyllabus->course_id = $cid;
         $courseSyllabus->build_id = 1;
         $courseSyllabus->chapter_id = $chid;
-        $courseSyllabus->seq = 0;
+        $courseSyllabus->seq = $seq;
         $courseSyllabus->save();
 
         return $courseSyllabus->id;
     }
 
+    function getNextSeq($cid)
+    {
+        $seq = CourseSyllabus::where('course_id', $cid)->max('seq');
+        return $seq ? ++$seq : 1;
+    }
 }
