f5339894d0921f8b770b52ebb7308651f1a316ee
Sławomir Borkowski <slawomir@bytech.ca>
2023-12-05 02:43:39 +0100

    gravatar based user logins log

--- resources/webcourses/src/app/core/services/gravatar-md5/gravatar-md5.service.ts
+++ resources/webcourses/src/app/core/services/gravatar-md5/gravatar-md5.service.ts
@@ -6,18 +6,10 @@ import { Md5 } from 'ts-md5/dist/md5'
 })
 export class GravatarMd5Service {
 
-  //     Used for default avatar when user is not logged in.
-  //     Requires full https path. We can use it again once URL is available from config
-  // private defaultAvatar = encodeURI('https://www.cultivatelearning.ca/assets/avatar.png')
-
   constructor() { }
 
-  gravatarMD5src(email) {
-    const md5email = Md5.hashStr(email)
-    const gravatarSrc = 'https://www.gravatar.com/avatar/' + md5email
-    // See note above definition for defaultAvatar
-    // const gravatarSrc = 'https://www.gravatar.com/avatar/' + md5email + '?d=' + this.defaultAvatar
-    return gravatarSrc
+  gravatarMD5src(email: string){
+    return 'https://www.gravatar.com/avatar/' + Md5.hashStr(email)
   }
 
 }
--- resources/webcourses/src/app/pages/admin/system/dashboard/dashboard.component.ts
+++ resources/webcourses/src/app/pages/admin/system/dashboard/dashboard.component.ts
@@ -3,12 +3,14 @@ import { Observable } from 'rxjs'
 import { faRepeat } from '@fortawesome/free-solid-svg-icons'
 
 // WNGX imports
-import { FadeInOut } from 'src/app/core/animations/fade-in-out.animation'
 import { DashboardService } from './dashboard.service'
+import { UserService } from 'src/app/core/services/user/user.service'
+import { GravatarMd5Service } from 'src/app/core/services/gravatar-md5/gravatar-md5.service'
 
 import { JetstreamUser } from 'src/app/core/models/jetstream-user.model'
 import { Coupon } from 'src/app/models/coupon.model'
-import { UserService } from 'src/app/core/services/user/user.service'
+
+import { FadeInOut } from 'src/app/core/animations/fade-in-out.animation'
 
 @Component({
   selector: 'app-dashboard',
@@ -30,7 +32,8 @@ export class DashboardComponent {
 
   constructor(
     public dashboardService: DashboardService,
-    public userService: UserService
+    public userService: UserService,
+    public gravatarMd5Service: GravatarMd5Service
   ) { }
 
   refresh() {
