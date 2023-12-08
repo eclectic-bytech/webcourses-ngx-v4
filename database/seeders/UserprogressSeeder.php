c6e4bab9a95cebad5bcc373429083f078524ecbf
Sławomir Borkowski <slawomir@bytech.ca>
2023-12-08 14:37:45 +0100

    added selected_aid to user_progress seeder

--- database/seeders/UserprogressSeeder.php
+++ database/seeders/UserprogressSeeder.php
@@ -19,16 +19,10 @@ class UserprogressSeeder extends Seeder
             'user_id' => '1',
             'course_id' => '1',
             'build_id' => '1',
+            'selected_aid' => '3964',
             'created_at' => '2022-01-23 14:38:23',
             'demo' => '0'
         ]);
 
-        DB::table('user_progress')->insert([
-            'user_id' => '1',
-            'course_id' => '2',
-            'build_id' => '1',
-            'created_at' => '2022-01-23 14:38:23',
-            'demo' => '0'
-        ]);
     }
 }
