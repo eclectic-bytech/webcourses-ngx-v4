b9c69d4a0f5e8d19ed8272562b94c719602171cc
Sławomir Borkowski <slawomir@bytech.ca>
2023-12-13 00:31:36 +0100

    sdk install and init updates

--- init-sdk.sh
+++ init-sdk.sh
@@ -1,37 +1,47 @@
 echo "Laravel migration is starting...";
 php ~/code/artisan migrate;
 echo
+
 echo "Copying Angular index.html to angular.blade.php....";
 cp ~/code/public/webcourses/index.html ~/code/resources/views/angular.blade.php;
 echo
+
 echo "Copying config.default.json to config.json....";
-echo
 cp ~/code/resources/webcourses/src/config.default.json ~/code/public/webcourses/config.json;
 echo
+
 echo "Seeding default user team...";
 php ~/code/artisan db:seed --class=TeamSeeder;
 echo
+
 echo "Seeding default roles...";
 php ~/code/artisan db:seed --class=RoleSeeder;
 echo
+
 echo "Seeding default admin user...";
 php ~/code/artisan db:seed --class=UserSeeder;
 echo
+
 echo "Seeding default publisher...";
 php ~/code/artisan db:seed --class=PublisherSeeder;
 echo
+
 echo "Seeding activity types...";
 php ~/code/artisan db:seed --class=ActivitytypeSeeder;
 echo
+
 echo "Seeding default course...";
 php ~/code/artisan db:seed --class=DemocourseSeeder;
 echo
+
 echo "Seeding default user progress entry...";
 php ~/code/artisan db:seed --class=UserprogressSeeder;
 echo
+
 echo "Seeding default user roles entries...";
 php ~/code/artisan db:seed --class=UserrolesSeeder;
 echo
+
 echo -e "\e[1;32m*******************************************************\e[0m";
 echo -e "\e[1;32m*\e[0m    \e[1mMake sure you have the following DNS entry:\e[0m      \e[1;32m*";
 echo -e "\e[1;32m*\e[0m    \e[34mhomestead.test 192.168.52.52                     \e[1;32m*";
--- install-sdk.sh
+++ install-sdk.sh
@@ -1,4 +1,4 @@
-echo -e "\033[1;35mWNGX Installer (SDK) v1.2.0a\033[1;34m";
+echo -e "\033[1;35mWNGX Installer (SDK) v1.3.0a\033[1;34m";
 
 echo -e "Running Composer...";
 composer install;
@@ -18,25 +18,26 @@ npm install;
 
 echo
 echo -e "Initialising demo publisher-files git submodule...";
+# Since /public/webcourses/publisher-files is in gitignore, we need the -f flag
 git submodule add -f https://github.com/eclectic-bytech/publisher-files ./public/webcourses/publisher-files
 
-echo
-echo -e "Compiling webcourses (Angular framework)...";
-npm run build:prod;
+# echo
+# echo -e "Compiling webcourses (Angular framework)...";
+# npm run build:prod;
 
 cd ../../
-
 echo
+
 echo "Running homestead make...";
 vendor/bin/homestead make;
-
 echo
+
 echo -e "Try dos2unix on init.sh to prevent console line break errors...";
 dos2unix init.sh;
-
 echo
+
 echo -e "\033[1;34mVagrant up...\033[0;37m";
 vagrant up;
 
 echo -e "\033[1;34mInit system via vagrant ssh command...";
-vagrant ssh --command "~/code/init.sh";
+vagrant ssh --command "~/code/init-sdk.sh";
