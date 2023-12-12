e8ec7d51c618c7f66294d6f3673a1d2ac4dc7ff3
SirL3X0N <seth@bytech.ca>
2023-12-12 15:23:29 +0100

    Added the button

--- resources/webcourses/src/app/pages/catalogue/course/course.module.ts
+++ resources/webcourses/src/app/pages/catalogue/course/course.module.ts
@@ -4,6 +4,7 @@ import { CourseComponent } from './course.component'
 import { CourseRoutingModule } from './course-routing.module'
 import { LoadingSpinnerModule } from '../../../core/modules/loading-spinner/loading-spinner.module'
 import { ThemeModule } from '../../../views/theme/theme.module'
+import { StripeCheckoutModule } from 'src/app/commerce/stripe/stripe-checkout/stripe-checkout.module'
 
 @NgModule({
   declarations: [CourseComponent],
@@ -11,7 +12,8 @@ import { ThemeModule } from '../../../views/theme/theme.module'
     CommonModule,
     CourseRoutingModule,
     LoadingSpinnerModule,
-    ThemeModule
+    ThemeModule,
+    StripeCheckoutModule
   ]
 })
 
