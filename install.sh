b5a57c81f2d6480e2652978ad1cd792640c68617
Sławomir Borkowski <slawomir@bytech.ca>
2023-12-13 00:32:10 +0100

    install.sh with init.sh merged into it

--- install.sh
+++ install.sh
@@ -6,13 +6,55 @@ composer install;
 echo -e "Creating a copy of .env file...";
 cp .env.example .env;
 
+echo "Copying config.default.json to config.json....";
+cp resources/webcourses/src/config.default.json ./public/webcourses/config.json;
+
 echo -e "Creating a copy of .htaccess file...";
 cp public/.htaccess_default public/.htaccess;
 
-echo
 echo -e "Initialising demo publisher-files git submodule...";
+# Since /public/webcourses/publisher-files is in gitignore, we need the -f flag
 git submodule add -f https://github.com/eclectic-bytech/publisher-files ./public/webcourses/publisher-files
 
+# --- INIT.SH -----------------------------------------
+
+echo "Laravel migration is starting...";
+php artisan migrate;
 echo
-echo "Copying config.default.json to config.json....";
-cp resources/webcourses/src/config.default.json ./public/webcourses/config.json;
+echo "Copying Angular index.html to angular.blade.php....";
+cp public/webcourses/index.html resources/views/angular.blade.php;
+echo
+echo "Seeding default user team...";
+php artisan db:seed --class=TeamSeeder;
+echo
+echo "Seeding default roles...";
+php artisan db:seed --class=RoleSeeder;
+echo
+echo "Seeding default admin user...";
+php artisan db:seed --class=UserSeeder;
+echo
+echo "Seeding default publisher...";
+php artisan db:seed --class=PublisherSeeder;
+echo
+echo "Seeding activity types...";
+php artisan db:seed --class=ActivitytypeSeeder;
+echo
+echo "Seeding default course...";
+php artisan db:seed --class=DemocourseSeeder;
+echo
+echo "Seeding default user progress entry...";
+php artisan db:seed --class=UserprogressSeeder;
+echo
+echo "Seeding default user roles entries...";
+php artisan db:seed --class=UserrolesSeeder;
+
+echo
+echo -e "\e[1;32m*******************************************************\e[0m";
+echo -e "\e[1;32m*                                                     *";
+echo -e "\e[1;32m*\e[0m        \e[1mUpdate your config files:\e[0m                    \e[1;32m*";
+echo -e "\e[1;32m*                                                     *";
+echo -e "\e[1;32m*\e[0m            - \e[34m.env                                   \e[1;32m*";
+echo -e "\e[1;32m*\e[0m            - \e[34mpublic/webcourses/config.json          \e[1;32m*";
+echo -e "\e[1;32m*                                                     *";
+echo -e "\e[1;32m*******************************************************\e[0m";
+echo
