dca83124380adb1a33deaebb764bf54d626e15d3
SirL3X0N <seth@bytech.ca>
2023-12-19 02:52:16 +0100

    Merged buttons and added if to change button names

--- resources/webcourses/src/app/pages/components/buttons/enter-course/enter-course.component.ts
+++ resources/webcourses/src/app/pages/components/buttons/enter-course/enter-course.component.ts
@@ -1,11 +1,25 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
   selector: 'wngx-enter-course',
   templateUrl: './enter-course.component.html',
   styleUrls: ['./enter-course.component.scss']
 })
-export class EnterCourseComponent {
+export class EnterCourseComponent implements OnInit {
   @Input() course: any
+  buttonToDisplay: 'Start' | 'Enter' | 'Resume'
 
+  ngOnInit() {
+    this.displayedButton()
+  }
+
+  displayedButton() {
+    if (this.course.user_progress.total_activities_completed === 0) {
+      this.buttonToDisplay = 'Start'
+    } else if (this.course.user_progress.total_activities_completed === this.course.total_activities) {
+      this.buttonToDisplay = 'Enter'
+    } else {
+      this.buttonToDisplay = 'Resume'
+    }
+  }
 }
