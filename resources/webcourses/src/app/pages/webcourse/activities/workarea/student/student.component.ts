9789154134f3812c846cf80f9fa38440a279f05d
Sławomir Borkowski <slawomir@bytech.ca>
2023-12-21 12:13:30 +0100

    added workarea/student module

---
+++ resources/webcourses/src/app/pages/webcourse/activities/workarea/student/student.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wngx-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.scss']
+})
+export class StudentComponent {
+
+}
---
+++ resources/webcourses/src/app/pages/webcourse/activities/workarea/student/student.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentComponent } from './student.component';
+
+
+
+@NgModule({
+  declarations: [
+    StudentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StudentModule { }
