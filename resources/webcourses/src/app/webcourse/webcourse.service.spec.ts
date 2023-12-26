cee88289e6e33eb358b68243017852d5711d102a
Sławomir Borkowski <slawomir@bytech.ca>
2023-12-26 12:58:58 +0100

    typos

--- resources/webcourses/src/app/webcourse/webcourse.service.spec.ts
+++ resources/webcourses/src/app/webcourse/webcourse.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { WebcourseService } from './webcourse.service';
+import { WebcourseService } from './webcourse.service'
 
 describe('WebcourseService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: WebcourseService = TestBed.get(WebcourseService);
+    const service: WebcourseService = TestBed.get(WebcourseService)
     expect(service).toBeTruthy();
   });
 });
