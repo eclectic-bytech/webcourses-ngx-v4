1d6f1be02ce51ed58f05a4cfff22e8eb327500d0
SirL3X0N <seth@bytech.ca>
2024-01-18 09:57:33 +0100

    got the import working

--- resources/webcourses/src/app/core/components/pagination-custom/pagination-custom.component.ts
+++ resources/webcourses/src/app/core/components/pagination-custom/pagination-custom.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'wngx-pagination-custom',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core'
 })
 
 export class PaginationCustomComponent {
-
+  @Input() codeUses: any
 }
