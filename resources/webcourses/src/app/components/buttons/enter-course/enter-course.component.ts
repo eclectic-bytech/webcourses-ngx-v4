4a6f298b726564081ab92425a40fe14631987780
Sławomir Borkowski <slawomir@bytech.ca>
2024-01-19 13:16:10 +0100

    turned function into getter

--- resources/webcourses/src/app/components/buttons/enter-course/enter-course.component.ts
+++ resources/webcourses/src/app/components/buttons/enter-course/enter-course.component.ts
@@ -10,7 +10,7 @@ export class EnterCourseComponent {
   @Input() course: any
   @Input() public btnSize: string
 
-  buttonLabel() {
+  get buttonLabel() {
     if (this.course.user_progress.total_activities_completed === 0) {
       return 'Start'
     } else if (this.course.user_progress.total_activities_completed === this.course.total_activities) {
